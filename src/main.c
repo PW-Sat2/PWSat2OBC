a27271951ad432139e31e7b4f66fdd1d3c961a39
Novakov <maciejt.nowak@gmail.com>
2015-12-09 20:11:09 +0100

    Tidy up test project

--- src/main.c
+++ src/main.c
@@ -1,22 +1,4 @@
-#include "stm32f4xx_rcc.h"
-#include "stm32f4xx_gpio.h"
-
 int main(void) {
-  GPIO_InitTypeDef  GPIO_InitStructure;
-  /* GPIOD Periph clock enable */
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-  /* Configure PD12, PD13, PD14 and PD15 in output push-pull mode */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-  GPIO_Init(GPIOD, &GPIO_InitStructure);
-
-  GPIO_SetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13);
-  GPIO_ResetBits(GPIOD, GPIO_Pin_14 | GPIO_Pin_15);
-
   while(1) {}
   return 1;
 }
