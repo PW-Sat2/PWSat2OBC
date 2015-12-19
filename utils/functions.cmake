a5f264aa1855d62ce0aecdf54c4a6410d702b88c
Novakov <maciejt.nowak@gmail.com>
2015-12-19 12:47:49 +0100

    Better semihosting handling

--- utils/functions.cmake
+++ utils/functions.cmake
@@ -18,3 +18,14 @@ function(target_jlink_flash TARGET COMMAND_FILE)
   )
 
 endfunction(target_jlink_flash)
+
+function(target_require_semihosting TARGET)
+  get_property(flags TARGET ${TARGET} PROPERTY LINK_FLAGS)
+  set_target_properties(${TARGET} PROPERTIES LINK_FLAGS "${flags} -specs=rdimon.specs")
+endfunction(target_require_semihosting)
+
+function(target_support_semihosting TARGET)
+  if(SEMIHOSTING)
+    target_require_semihosting(${TARGET})
+  endif()
+endfunction(target_support_semihosting)
