73a12c5072d3aa37ef168aa2d699ba2f9941fd22
Novakov <maciejt.nowak@gmail.com>
2016-01-26 10:00:59 +0100

    Fixed building UT under linux & gtest under Windows

--- test/semihosting.cpp
+++ test/semihosting.cpp
@@ -12,6 +12,7 @@ extern int run(int argc, char** argv);
 extern "C" {
 	extern void initialise_monitor_handles(void);
 	extern void __libc_init_array(void);
+	extern int kill(pid_t, int);
 }
 
 typedef struct {
