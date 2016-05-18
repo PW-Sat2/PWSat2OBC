63961aa8767cc17c3f6a4b1e60b53ef5ebd32609
Ernest Szczekocki <ernest.szczekocki@outlook.com>
2016-05-18 22:44:45 +0200

    using freertos delay and move adxrs pins location to io_map.h

--- src/io_map.h
+++ src/io_map.h
@@ -12,4 +12,20 @@
 #define LEUART0_LOCATION LEUART_ROUTE_LOCATION_LOC0
 #define LEUART0_BAUDRATE 9600
 
+#define ADXRS453_SPI                                              \
+{                                                                         \
+  USART1,                       /* USART port                       */    \
+  _USART_ROUTE_LOCATION_LOC1,   /* USART pins location number ||  MOSI-PD0, MISO-PD1, CLK-PD2, CS-PD3       */    \
+  1000000,                      /* Bitrate                          */    \
+  8,                            /* Frame length                     */    \
+  0,                            /* Dummy tx value for rx only funcs */    \
+  spidrvMaster,                 /* SPI mode                         */    \
+  spidrvBitOrderMsbFirst,       /* Bit order on bus                 */    \
+  spidrvClockMode0,             /* SPI clock/phase mode             */    \
+  spidrvCsControlAuto,          /* CS controlled by the driver      */    \
+  spidrvSlaveStartDelayed          \
+}
+
+
+
 #endif
