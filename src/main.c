914ac4426d8575c1b60344c43946cd0d72f07352
Ernest Szczekocki <ernest.szczekocki@outlook.com>
2016-05-19 13:56:16 +0200

    cleaning

--- src/main.c
+++ src/main.c
@@ -49,19 +49,13 @@ void blinkLed0(void * param)
 void ADXRS(void * param){
 	UNREFERENCED_PARAMETER(param);
 
-
-
-	long temp=0;  //i know it should be a float but swoPrintf has a problem with %f
+	float temp=0;
 	float rate=0;
-	float angle=0;
-	uint8_t timeDelay=0;
 	ADXRS453_Init();
-
 	while(1){
 		rate=ADXRS453_GetRate();
-
 		temp=ADXRS453_GetTemperature();
-		swoPrintf("temp: %d ' celcius rate: %d '/sec rotation\n", temp, (long)rate);
+		swoPrintf("temp: %d ' celcius rate: %d '/sec rotation\n", (long)temp, (long)rate);
 		vTaskDelay(10 / portTICK_PERIOD_MS);
 	}
 
@@ -73,7 +67,6 @@ int main(void)
 	CHIP_Init();
 	CMU_ClockSelectSet(cmuClock_LFA, cmuSelect_LFXO);
 	CMU_ClockSelectSet(cmuClock_LFB, cmuSelect_LFXO);
-	CMU_ClockEnable ( cmuClock_TIMER0, true );
 	CMU_ClockEnable(cmuClock_GPIO, true);
 	enableSWO();
 
