08370fed1beaa8e42c38757bc5c0351c75d51991
Rafał Tymiński <tyminski.rafal@gmail.com>
2016-06-15 19:25:41 +0200

    Initial commit for uart driver

---
+++ libs/base/io_map.h
@@ -0,0 +1,31 @@
+#ifndef SRC_IO_MAP_H_
+#define SRC_IO_MAP_H_
+
+#include <em_system.h>
+
+#define LED_PORT gpioPortE
+#define LED0 2
+#define LED1 3
+
+// LEUART0
+#define LEUART0_PORT gpioPortD
+#define LEUART0_TX 4
+#define LEUART0_RX 5
+#define LEUART0_LOCATION LEUART_ROUTE_LOCATION_LOC0
+#define LEUART0_BAUDRATE 9600
+
+// I2C
+#define I2C I2C1
+#define I2C_PORT gpioPortC
+#define I2C_SDA_PIN 4
+#define I2C_SCL_PIN 5
+#define I2C_LOCATION I2C_ROUTE_LOCATION_LOC0
+
+// USART1
+#define USART1_PORT gpioPortD
+#define USART1_TX 0
+#define USART1_RX 1
+#define UART_LOCATION UART_ROUTE_LOCATION_LOC1
+#define USART1_BAUDRATE 9600
+
+#endif
