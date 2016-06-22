a6c3c54f94825a13696bd0eaaeeb9a84b3b59457
Novakov <maciejt.nowak@gmail.com>
2016-06-22 16:11:07 +0200

    Potential fix for COM Access Denied

--- integration_tests/i2cMock/i2cMock.py
+++ integration_tests/i2cMock/i2cMock.py
@@ -1,3 +1,4 @@
+import traceback
 from threading import Thread
 
 import serial
@@ -45,6 +46,7 @@ class I2CDevice(object):
         if handler is None:
             return self._missing_handler(data)
 
+
         self.response = handler(self, *args)
 
     def read(self):
@@ -85,7 +87,14 @@ class I2CMock(object):
     devices = {}
 
     def __init__(self, port_name, baudrate=250000):
-        self.port = serial.Serial(port=port_name, baudrate=baudrate, rtscts=True, write_timeout=None)
+        self.port = None
+        while self.port is None:
+            try:
+                self.port = serial.Serial(port=port_name, baudrate=baudrate, rtscts=True, write_timeout=None)
+            except serial.SerialException as e:
+                if not e.args[0].endswith("WindowsError(5, 'Access is denied.')"):
+                    raise
+
         self.active = False
         self.reader = Thread(target=I2CMock._run, args=(self,))
         self.reader.daemon = True
@@ -107,6 +116,7 @@ class I2CMock(object):
     def close(self):
         self.port.close()
         self.reader.join()
+        self.port.close()
         self.port = None
 
     def restart(self):
@@ -116,18 +126,26 @@ class I2CMock(object):
         while c != 'X':
             c = self.port.read(1)
 
+    def _read_port(self, count=1):
+        try:
+            return self.port.read(count)
+        except AttributeError:
+            return None
+
     def _run(self):
         self.log.debug("Worker thread starting")
         while self.port.is_open:
             try:
-                c = self.port.read(1)
-
-                self.handle_command(c)
+                c = self._read_port(1)
+                if c is not None:
+                    self.handle_command(c)
             except serial.SerialException as e:
                 self.log.error("Serial read exception %r. Breaking", e)
                 break
+            except AttributeError:
+                pass
             except Exception as e:
-                self.log.error("Serial read exception %r", e)
+                self.log.error("Handle command exception %r", e)
 
         self.log.debug("Finished worker thread")
 
@@ -147,21 +165,24 @@ class I2CMock(object):
             return MissingDevice(address)
 
     def _device_selected_for_write(self):
-        address = ord(self.port.read(1))
+        address = ord(self._read_port(1))
 
         self.log.debug("Device %X selected for write", address)
 
-        number_of_bytes = ord(self.port.read(1))
+        number_of_bytes = ord(self._read_port(1))
 
         self.log.debug("Number of bytes to write: %d", number_of_bytes)
 
-        data = [ord(self.port.read()) for _ in range(number_of_bytes)]
+        data = [ord(self._read_port()) for _ in range(number_of_bytes)]
 
         self.log.debug("Written bytes: %s", map(lambda x: "%X" % x, data))
 
         device = self._device(address)
 
-        device.handle(data)
+        try:
+            device.handle(data)
+        except Exception as e:
+            self.log.error("Handle exception: %r", e)
 
     def _escape(self, data):
         def escape_char(c):
@@ -175,7 +196,7 @@ class I2CMock(object):
         return [x for y in escaped for x in y]
 
     def _device_selected_for_read(self):
-        address = ord(self.port.read(1))
+        address = ord(self._read_port(1))
 
         self.log.debug("Device %X selected for read", address)
 
