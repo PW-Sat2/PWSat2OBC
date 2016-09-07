15595973a799762443de2456359070a7ad6a3bb3
Łukasz Żak <zaklukasz32@gmail.com>
2016-09-07 22:15:44 +0200

    [terminal] Fix ping commands.

--- src/commands/ping.c
+++ src/commands/ping.c
@@ -5,23 +5,24 @@
 #include <task.h>
 
 #include "commands.h"
+#include "obc.h"
 #include "system.h"
-#include "terminal.h"
+#include "terminal/terminal.h"
 
 void PingHandler(uint16_t argc, char* argv[])
 {
     UNREFERENCED_PARAMETER(argc);
     UNREFERENCED_PARAMETER(argv);
 
-    TerminalPuts("pong");
+    TerminalPuts(&Main.terminal, "pong");
 }
 
 void EchoHandler(uint16_t argc, char* argv[])
 {
-    TerminalPuts("echo with args: \r\n");
+    TerminalPuts(&Main.terminal, "echo with args: \r\n");
 
     for (int i = 0; i < argc; i++)
     {
-        TerminalPrintf("%d. %s \r\n", i, argv[i]);
+        TerminalPrintf(&Main.terminal, "%d. %s \r\n", i, argv[i]);
     }
 }
