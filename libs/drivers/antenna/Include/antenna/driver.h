ae60d863aacaf7c2bad37c95136419311fa4d132
Łukasz Żak <zaklukasz32@gmail.com>
2016-09-15 07:23:32 +0200

    [antenna] First draft of antenna miniport driver.
    * Extend buffer reader with 16 big endian word support.
    * Add first version of the miniport driver.

--- libs/drivers/antenna/Include/antenna/driver.h
+++ libs/drivers/antenna/Include/antenna/driver.h
@@ -4,16 +4,46 @@
 #pragma once
 
 #include "antenna.h"
-#include "system.h"
+#include "base/os.h"
+#include "time/TimePoint.h"
 
-EXTERNC_BEGIN
-
-typedef enum {
+enum AntenaPortStatus
+{
     ANTENNA_PORT_OPERATIONAL,
     ANTENNA_PORT_FAILURE,
-} AntenaPortStatus;
+};
 
-typedef struct
+enum TelemetryField
+{
+    ANT_TM_ANTENNA1_DEPLOYMENT_STATUS = 1 << 0,
+    ANT_TM_ANTENNA2_DEPLOYMENT_STATUS = 1 << 1,
+    ANT_TM_ANTENNA3_DEPLOYMENT_STATUS = 1 << 2,
+    ANT_TM_ANTENNA4_DEPLOYMENT_STATUS = 1 << 3,
+
+    ANT_TM_ANTENNA1_DEPLOYMENT_ACTIVE = 1 << 4,
+    ANT_TM_ANTENNA2_DEPLOYMENT_ACTIVE = 1 << 5,
+    ANT_TM_ANTENNA3_DEPLOYMENT_ACTIVE = 1 << 6,
+    ANT_TM_ANTENNA4_DEPLOYMENT_ACTIVE = 1 << 7,
+
+    ANT_TM_ANTENNA1_ACTIVATION_COUNT = 1 << 8,
+    ANT_TM_ANTENNA2_ACTIVATION_COUNT = 1 << 9,
+    ANT_TM_ANTENNA3_ACTIVATION_COUNT = 1 << 10,
+    ANT_TM_ANTENNA4_ACTIVATION_COUNT = 1 << 11,
+
+    ANT_TM_ANTENNA1_ACTIVATION_TIME = 1 << 12,
+    ANT_TM_ANTENNA2_ACTIVATION_TIME = 1 << 13,
+    ANT_TM_ANTENNA3_ACTIVATION_TIME = 1 << 14,
+    ANT_TM_ANTENNA4_ACTIVATION_TIME = 1 << 15,
+
+    ANT_TM_TEMPERATURE1 = 1 << 16,
+    ANT_TM_TEMPERATURE2 = 1 << 17,
+    ANT_TM_SWITCHES_IGNORED1 = 1 << 18,
+    ANT_TM_SWITCHES_IGNORED2 = 1 << 19,
+    ANT_TM_DEPLOYMENT_SYSTEM_STATUS1 = 1 << 20,
+    ANT_TM_DEPLOYMENT_SYSTEM_STATUS2 = 1 << 21,
+};
+
+struct AntennaTelemetry
 {
     bool DeploymentStatus[4];
     bool IsDeploymentActive[4];
@@ -22,16 +52,19 @@ typedef struct
     uint16_t Temperature;
     bool IgnoringDeploymentSwitches;
     bool DeploymentSystemArmed;
-} AntennaTelemetry;
+    uint32_t flags;
+};
 
-typedef struct AntennaDriver
+struct AntennaChannelInfo
 {
-    AntennaMiniportDriver* primaryPort;
-    AntennaMiniportDriver* secondaryPort;
-    AntenaPortStatus primaryPortStatus;
-    AntenaPortStatus secondaryPortStatus;
+    AntennaMiniportDriver* port;
+    AntenaPortStatus status;
+};
 
-    OSResult (*InitializeDeployment)(struct AntennaDriver* driver);
+struct AntennaDriver
+{
+    AntennaChannelInfo primaryChannel;
+    AntennaChannelInfo secondaryChannel;
 
     OSResult (*Reset)(struct AntennaDriver* driver);
 
@@ -45,15 +78,11 @@ typedef struct AntennaDriver
 
     OSResult (*InitializeAutomaticDeployment)(struct AntennaDriver* driver);
 
+    OSResult (*CancelAntennaDeployment)(struct AntennaDriver* driver);
+
+    OSResult (*GetTemperature)(struct AntennaDriver* driver, uint16_t* temperature);
+
     OSResult (*GetTelemetry)(struct AntennaDriver* driver, AntennaTelemetry* telemetry);
-
-} AntennaDriver;
-
-OSResult AntennaDriverInitialize(AntennaDriver* driver,
-    AntennaMiniportDriver* primary,
-    AntennaMiniportDriver* secondary //
-    );
-
-EXTERNC_END
+};
 
 #endif
