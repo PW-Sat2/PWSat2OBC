beaace46e49994b2dcbc56eedfeed830daff1e55
Novakov <maciejt.nowak@gmail.com>
2016-10-30 18:56:08 +0100

    Store Saleae dump in build directory

--- integration_tests/pyconsole.py
+++ integration_tests/pyconsole.py
@@ -4,7 +4,7 @@ import logging
 from system import System
 
 try:
-    from config import config
+    from build_config import config
 except ImportError as e:
     raise ImportError(
         "Error loading config: %s. Did you forget to add <build>/integration_tests to PYTHONPATH?" % e.message)
