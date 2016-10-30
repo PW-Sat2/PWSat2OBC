ce760e642c0d87baa1b112b21a2ee92aafe70eaa
Łukasz Żak <zaklukasz32@gmail.com>
2016-10-30 18:17:55 +0100

    [antenna] First draft of antenna controller simulator.

--- integration_tests/devices/__init__.py
+++ integration_tests/devices/__init__.py
@@ -1,4 +1,7 @@
 from eps import EPSDevice
 from comm import TransmitterDevice, ReceiverDevice
+from antenna import *
+from antenna import PRIMARY_ANTENNA_CONTROLLER_ADDRESS
+from antenna import BACKUP_ANTENNA_CONTROLLER_ADDRESS
 
-__all__ = ['EPSDevice', 'TransmitterDevice', 'ReceiverDevice']
+__all__ = ['EPSDevice', 'TransmitterDevice', 'ReceiverDevice', 'AntennaController', 'PRIMARY_ANTENNA_CONTROLLER_ADDRESS', 'BACKUP_ANTENNA_CONTROLLER_ADDRESS']
