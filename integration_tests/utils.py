73685a9fdef4d2c6d1f18358da512db55251dbbb
Novakov <maciejt.nowak@gmail.com>
2016-11-05 19:18:08 +0100

    Remaining OBC commands moved to mixins

--- integration_tests/utils.py
+++ integration_tests/utils.py
@@ -3,3 +3,16 @@ def hex_data(data):
         data = [ord(c) for c in data]
 
     return str(['%X' % b for b in data])
+
+
+def pad_to_multiply(s, base):
+    l = base - len(s) % base
+    if l < base:
+        padding = '=' * l
+    else:
+        padding = ''
+    return s + padding
+
+
+def b64pad(s):
+    return pad_to_multiply(s, 3)
