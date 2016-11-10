a4627eb1fb3a08cffe4d5f384df5350308d666b5
Novakov <maciejt.nowak@gmail.com>
2016-11-10 20:31:50 +0100

    Telecommand handling draft

--- src/obc.cpp
+++ src/obc.cpp
@@ -1,8 +1,48 @@
 #include "obc.h"
 #include "io_map.h"
 
+using namespace std;
+
 OBC::OBC()
     : I2C(&I2CBuses[I2C_SYSTEM_BUS].ErrorHandling.Base, &I2CBuses[I2C_PAYLOAD_BUS].ErrorHandling.Base), //
+      Telecommands(comm),                                                                               //
+      AllTelecommands{&Telecommands.Ping},                                                              //
+      FrameHandler(FrameUnpacker, FrameUnpacker, AllTelecommands, COUNT_OF(AllTelecommands)),           //
       comm(*I2C.Bus, FrameHandler)
+{
+}
+
+std::size_t TelecommandFrameUnpacker::Decrypt(gsl::span<const uint8_t> frame, gsl::span<uint8_t> decrypted)
+{
+    auto lastCopied = std::copy(frame.cbegin(), frame.cend(), decrypted.begin());
+
+    return lastCopied - decrypted.begin();
+}
+
+PingTelecommand::PingTelecommand(devices::comm::CommObject& comm) : _comm(comm)
+{
+}
+
+void TelecommandFrameUnpacker::Decode(gsl::span<const uint8_t> frame, uint8_t& commandCode, gsl::span<const uint8_t>& parameters)
+{
+    commandCode = frame[0];
+    parameters = frame.subspan(1, frame.length() - 1);
+}
+
+void PingTelecommand::Handle(gsl::span<const uint8_t> parameters)
+{
+    UNREFERENCED_PARAMETER(parameters);
+
+    const char* response = "PONG";
+
+    this->_comm.SendFrame(gsl::span<const uint8_t>(reinterpret_cast<const uint8_t*>(response), 4));
+}
+
+std::uint8_t PingTelecommand::CommandCode() const
+{
+    return static_cast<uint8_t>('P');
+}
+
+TelecommandsObject::TelecommandsObject(devices::comm::CommObject& comm) : Ping(comm)
 {
 }
---
+++ unit_tests/TeleCommandHandling/TeleCommandHandlingTest.cpp
@@ -0,0 +1,89 @@
+#include <algorithm>
+#include <cstdint>
+#include <gsl/span>
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "comm/comm.h"
+#include "comm_handling/comm_handling.h"
+#include "utils.hpp"
+
+using testing::Test;
+using testing::NiceMock;
+using testing::Return;
+using testing::Invoke;
+using testing::_;
+using testing::Eq;
+using testing::StrEq;
+using namespace std;
+using namespace devices::comm;
+
+struct TeleCommandDepsMock : public IDecryptFrame, public IDecodeTelecommand
+{
+    MOCK_METHOD3(Decode, void(gsl::span<const std::uint8_t> frame, std::uint8_t& commandCode, gsl::span<const std::uint8_t>& parameters));
+
+    MOCK_METHOD2(Decrypt, size_t(gsl::span<const uint8_t> frame, gsl::span<uint8_t> decrypted));
+};
+
+struct TeleCommandHandlerMock : public IHandleTeleCommand
+{
+    MOCK_METHOD1(Handle, void(gsl::span<const std::uint8_t> parameters));
+    MOCK_CONST_METHOD0(CommandCode, uint8_t());
+};
+
+class TeleCommandHandlingTest : public Test
+{
+  public:
+    TeleCommandHandlingTest();
+
+  protected:
+    IncomingTelecommandHandler handling;
+    NiceMock<TeleCommandDepsMock> deps;
+};
+
+TeleCommandHandlingTest::TeleCommandHandlingTest() : handling(deps, deps, nullptr, 0)
+{
+}
+
+TEST_F(TeleCommandHandlingTest, IncomingFrameShouldBeDecryptedAndDecoded)
+{
+    CommFrame frame;
+    frame.Size = 40;
+
+    EXPECT_CALL(this->deps, Decrypt(_, _)).WillOnce(Return(40));
+    EXPECT_CALL(this->deps, Decode(_, _, _));
+
+    this->handling.HandleFrame(frame);
+}
+
+TEST_F(TeleCommandHandlingTest, HandlerShouldBeCalledForKnownTelecommand)
+{
+    const char* contents = "ABCD";
+
+    CommFrame frame;
+    frame.Size = 50;
+    strcpy((char*)frame.Contents, contents);
+
+    EXPECT_CALL(this->deps, Decrypt(_, _))
+        .WillOnce(Invoke([](gsl::span<const uint8_t> frame, gsl::span<uint8_t> decrypted) {
+            auto lastCopied = std::copy(frame.cbegin(), frame.cend(), decrypted.begin());
+
+            return lastCopied - decrypted.begin();
+        }));
+    EXPECT_CALL(this->deps, Decode(_, _, _))
+        .WillOnce(Invoke([](gsl::span<const std::uint8_t> frame, std::uint8_t& commandCode, gsl::span<const std::uint8_t>& parameters) {
+            commandCode = frame[0];
+            parameters = frame.subspan(1, frame.length() - 1);
+        }));
+
+    NiceMock<TeleCommandHandlerMock> someCommand;
+    EXPECT_CALL(someCommand, Handle(_));
+    EXPECT_CALL(someCommand, CommandCode()).WillRepeatedly(Return(static_cast<uint8_t>('A')));
+
+    IHandleTeleCommand* commands[] = {&someCommand};
+
+    IncomingTelecommandHandler handler(deps, deps, commands, 1);
+
+    handler.HandleFrame(frame);
+}
