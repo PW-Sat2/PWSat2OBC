d8c1e5a4e794ba8f7fd8081b23d287f0d8b9b5eb
Novakov <maciejt.nowak@gmail.com>
2016-11-10 08:36:31 +0100

    Refactored command formatter class

--- integration_tests/obc/obc.py
+++ integration_tests/obc/obc.py
@@ -1,6 +1,7 @@
 import logging
+from base64 import b64encode
 
-from .CommandFormatter import CommandFormatter
+from utils import ExtendableFormatter
 from .obc_mixin import OBCMixin
 from .file_system import FileSystemMixin
 from .comm import CommMixin
@@ -17,7 +18,8 @@ class OBC(OBCMixin,
     def __init__(self, terminal):
         self.log = logging.getLogger("OBC")
 
-        self._formatter = CommandFormatter()
+        self._formatter = ExtendableFormatter()
+        self._formatter.register_conversion('E', lambda d: b64encode(d).rstrip('='))
 
         self._terminal = terminal
         self._terminal.reset()
--- integration_tests/utils.py
+++ integration_tests/utils.py
@@ -1,3 +1,6 @@
+from string import Formatter
+
+
 def hex_data(data):
     if isinstance(data, basestring):
         data = [ord(c) for c in data]
@@ -16,3 +19,16 @@ def pad_to_multiply(s, base):
 
 def b64pad(s):
     return pad_to_multiply(s, 3)
+
+
+class ExtendableFormatter(Formatter):
+    _converters = {}
+
+    def convert_field(self, value, conversion):
+        if conversion in self._converters:
+            return self._converters[conversion](value)
+
+        return super(ExtendableFormatter, self).convert_field(value, conversion)
+
+    def register_conversion(self, name, func):
+        self._converters[name] = func
