a1fd37007abfb0cb706f06175fb69ddc24bac9e9
Novakov <maciejt.nowak@gmail.com>
2016-11-23 22:31:11 +0100

    Update unit tests OS mock

--- unit_tests/os/os.hpp
+++ unit_tests/os/os.hpp
@@ -1,6 +1,8 @@
 #ifndef OS_INTERFACE_HPP
 #define OS_INTERFACE_HPP
 
+#include <cstdint>
+
 #pragma once
 
 #include "base/os.h"
@@ -9,9 +11,9 @@ struct IOS
 {
     virtual OSResult CreateTask(OSTaskProcedure entryPoint,
         const char* taskName,
-        uint16_t stackSize,
+        std::uint16_t stackSize,
         void* taskParameter,
-        uint32_t priority,
+        std::uint32_t priority,
         OSTaskHandle* taskHandle) = 0;
 
     virtual void SuspendTask(OSTaskHandle task) = 0;
@@ -37,11 +39,22 @@ struct IOS
     virtual OSEventBits EventGroupWaitForBits(
         OSEventGroupHandle eventGroup, const OSEventBits bitsToWaitFor, bool waitAll, bool autoReset, const OSTaskTimeSpan timeout) = 0;
 
+    virtual void* Alloc(std::size_t size) = 0;
+    virtual void Free(void* ptr) = 0;
+    virtual OSQueueHandle CreateQueue(std::size_t maxElementCount, std::size_t elementSize) = 0;
+    virtual bool QueueReceive(OSQueueHandle queue, void* element, OSTaskTimeSpan timeout) = 0;
+    virtual bool QueueReceiveFromISR(OSQueueHandle queue, void* element, bool* taskWoken) = 0;
+    virtual bool QueueSend(OSQueueHandle queue, void* element, OSTaskTimeSpan timeout) = 0;
+    virtual bool QueueSendISR(OSQueueHandle queue, void* element, bool* taskWoken) = 0;
+    virtual void QueueOverwrite(OSQueueHandle queue, const void* element) = 0;
+
     virtual OSPulseHandle CreatePulseAll() = 0;
 
     virtual OSResult PulseWait(OSPulseHandle handle, OSTaskTimeSpan timeout) = 0;
 
     virtual void PulseSet(OSPulseHandle handle) = 0;
+
+    virtual void EndSwitchingISR(bool taskWoken) = 0;
 };
 
 class OSReset
