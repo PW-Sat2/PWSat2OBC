1dfaccbdcb3853027cd75a928e6e01ac0e6c7a5b
Grzegorz Gajoch <gajoch@gmail.com>
2017-01-20 11:17:25 +0000

    CR fixes

--- unit_tests/base/RapidTest.cpp
+++ unit_tests/base/RapidTest.cpp
@@ -3,16 +3,11 @@
 #include "rapidcheck.hpp"
 #include "rapidcheck/gtest.h"
 
-#include "system.h"
 #include "base/reader.h"
 #include "base/writer.h"
 
-using testing::Eq;
-using testing::ElementsAre;
-
-
 template<typename WriterFunction, typename ReaderMemberPtr, typename V>
-void WriterReaderRapidCheckTest(WriterFunction w, ReaderMemberPtr r, V value)
+void WriteAndReadRapidCheckTest(WriterFunction w, ReaderMemberPtr r, V value)
 {
 	Writer writer;
 	std::array<uint8_t, sizeof(V)> array;
@@ -29,47 +24,47 @@ void WriterReaderRapidCheckTest(WriterFunction w, ReaderMemberPtr r, V value)
 	RC_ASSERT(reader.RemainingSize() == 0);
 }
 
-RC_GTEST_PROP(WriteReadRapicheck, Byte, (uint8_t value))
+RC_GTEST_PROP(WriteAndRead, Byte, (uint8_t value))
 {
-	WriterReaderRapidCheckTest(WriterWriteByte, &Reader::ReadByte, value);
+	WriteAndReadRapidCheckTest(WriterWriteByte, &Reader::ReadByte, value);
 }
 
-RC_GTEST_PROP(WriteReadRapicheck, WordLE, (uint16_t value))
+RC_GTEST_PROP(WriteAndRead, WordLE, (uint16_t value))
 {
-	WriterReaderRapidCheckTest(WriterWriteWordLE, &Reader::ReadWordLE, value);
+	WriteAndReadRapidCheckTest(WriterWriteWordLE, &Reader::ReadWordLE, value);
 }
 
-RC_GTEST_PROP(WriteReadRapicheck, SignedWordLE, (int16_t value))
+RC_GTEST_PROP(WriteAndRead, SignedWordLE, (int16_t value))
 {
-	WriterReaderRapidCheckTest(WriterWriteSignedWordLE, &Reader::ReadSignedWordLE, value);
+	WriteAndReadRapidCheckTest(WriterWriteSignedWordLE, &Reader::ReadSignedWordLE, value);
 }
 
-RC_GTEST_PROP(WriteReadRapicheck, DoubleWordLE, (uint32_t value))
+RC_GTEST_PROP(WriteAndRead, DoubleWordLE, (uint32_t value))
 {
-	WriterReaderRapidCheckTest(WriterWriteDoubleWordLE, &Reader::ReadDoubleWordLE, value);
+	WriteAndReadRapidCheckTest(WriterWriteDoubleWordLE, &Reader::ReadDoubleWordLE, value);
 }
 
-RC_GTEST_PROP(WriteReadRapicheck, SignedDoubleWordLE, (int32_t value))
+RC_GTEST_PROP(WriteAndRead, SignedDoubleWordLE, (int32_t value))
 {
-	WriterReaderRapidCheckTest(WriterWriteSignedDoubleWordLE, &Reader::ReadSignedDoubleWordLE, value);
+	WriteAndReadRapidCheckTest(WriterWriteSignedDoubleWordLE, &Reader::ReadSignedDoubleWordLE, value);
 }
 
-RC_GTEST_PROP(WriteReadRapicheck, QuadWordLE, (uint64_t value))
+RC_GTEST_PROP(WriteAndRead, QuadWordLE, (uint64_t value))
 {
-	WriterReaderRapidCheckTest(WriterWriteQuadWordLE, &Reader::ReadQuadWordLE, value);
+	WriteAndReadRapidCheckTest(WriterWriteQuadWordLE, &Reader::ReadQuadWordLE, value);
 }
 
-RC_GTEST_PROP(WriteReadRapicheck, Array, (std::vector<uint8_t> value))
+RC_GTEST_PROP(WriteAndRead, Array, (std::vector<uint8_t> value))
 {
 	RC_PRE(value.size() > 0u);
-	std::unique_ptr<uint8_t[]> array(new uint8_t[value.size()]);
+	std::vector<uint8_t> array(value.size());
 
 	Writer writer;
-	WriterInitialize(&writer, array.get(), value.size());
+	WriterInitialize(&writer, array.data(), array.size());
 	RC_ASSERT(WriterWriteArray(&writer, value.data(), value.size()));
 	RC_ASSERT(WriterStatus(&writer));
 
-	Reader reader{gsl::make_span(array.get(), value.size())};
+	Reader reader{array};
 	auto read = reader.ReadArray(value.size());
 	for(size_t i = 0; i < value.size(); ++i)
 	{
