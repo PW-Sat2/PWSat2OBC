9ed9854c945ec29897752fdc17963b6bc2520dcf
pweclewski <weclewski.piotr@gmail.com>
2017-02-01 01:17:04 +0000

    split to two tests, added test checking dynamic allocation block

--- unit_tests/eigen/eigenTest.cpp
+++ unit_tests/eigen/eigenTest.cpp
@@ -1,35 +1,42 @@
-#include <string>
-#include <tuple>
-#include "gtest/gtest.h"
-#include "gmock/gmock.h"
-#include "gmock/gmock-matchers.h"
-#include "rapidcheck.hpp"
-#include "rapidcheck/gtest.h"
-#include "utils.hpp"
-
 #include <iostream>
-#include <Eigen/Dense>
+#include "gtest/gtest.h"
 
-using testing::_;
-using testing::Eq;
-using testing::Ne;
-using testing::Ge;
-using testing::StrEq;
-using testing::Return;
-using testing::Invoke;
-using testing::Pointee;
-using testing::ElementsAre;
-using testing::Matches;
+// define custom assert macro for eigen throwing custom type -
+//- must be placed before eigen inclusion
+typedef int EIGENUT_EXCP_TYPE;
+#define eigen_assert(x) if(!(x)){throw EIGENUT_EXCP_TYPE(1);}
+
+#include <Eigen/Dense>
 
 using Eigen::Matrix3d;
 using Eigen::Vector3d;
 
+
+// testing creation of basic types by static allocation
 TEST(eigen_sanity, MatVecCreation)
 {
-    Matrix3d m = Matrix3d::Identity();
-    m = (m + Matrix3d::Constant(1.2)) * 50;
-    Vector3d v(1.0, 2.0, 3.0);
-    EXPECT_FLOAT_EQ(v[0], 1.0);
-    EXPECT_FLOAT_EQ(m(0, 2), 60.0);
-    EXPECT_FLOAT_EQ(m(2, 2), 110.0);
+  Matrix3d m = Matrix3d::Identity();
+  m = (m + Matrix3d::Constant(1.2)) * 50;
+  Vector3d v(1.0, 2.0, 3.0);
+  EXPECT_FLOAT_EQ(v[0], 1.0);
+  EXPECT_FLOAT_EQ(m(0, 2), 60.0);
+  EXPECT_FLOAT_EQ(m(2, 2), 110.0);
+}
+
+// function using dynamic allocation - must throw exception from
+//custom defined eigen_assert function if EIGEN_NO_MALLOC is defined
+void throwMallocAssert()
+{
+  int size = 50;
+  // VectorXf is a vector of floats, with dynamic size.
+  Eigen::VectorXf u(size), z(size), w(size);
+  u = z + w;
+}
+
+// test block of dynamic memory allocation  (EIGEN_NO_MALLOC)
+TEST(eigen_sanity, NoDynAlloc)
+{
+  std::cout << "[***INFO***] EigenUT: \"GLib-WARNING\" is expected due to EIGEN_NO_MALLOC testing." << std::endl;
+
+  EXPECT_THROW(throwMallocAssert(), EIGENUT_EXCP_TYPE);
 }
