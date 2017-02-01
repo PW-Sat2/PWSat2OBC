c4b5685b81b112d02154d0512cc2e161134bfcdb
Marcin Drobik <mdrobik@future-processing.com>
2017-02-01 20:51:41 +0100

    BURTC Integration tests

--- integration_tests/tests/test_time.py
+++ integration_tests/tests/test_time.py
@@ -1,6 +1,6 @@
 from system import wait_for_obc_start
 from tests.base import BaseTest
-
+import time
 
 class TestTime(BaseTest):
     @wait_for_obc_start()
@@ -15,3 +15,18 @@ class TestTime(BaseTest):
 
         self.assertGreaterEqual(current_time, 100)
 
+    @wait_for_obc_start()
+    def test_clock_running(self):
+        wait_time_s = 3
+        wait_time_accuracy_s = 1
+
+        start_time = self.system.obc.current_time()
+
+        time.sleep(wait_time_s)
+
+        end_time = self.system.obc.current_time()
+
+        time_difference = end_time - start_time
+
+        self.assertGreaterEqual(time_difference, wait_time_s - wait_time_accuracy_s)
+        self.assertLessEqual(time_difference, wait_time_s + wait_time_accuracy_s)
