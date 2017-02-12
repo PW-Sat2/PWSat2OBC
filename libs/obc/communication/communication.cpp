1a595eaf1af20266fa386fb8d89a74474db7a116
Novakov <maciejt.nowak@gmail.com>
2017-02-12 19:44:03 +0100

    Moved telecommands implementation to separate library

--- libs/obc/communication/communication.cpp
+++ libs/obc/communication/communication.cpp
@@ -6,31 +6,9 @@
 using std::uint8_t;
 using gsl::span;
 using drivers::i2c::II2CBus;
-using devices::comm::ITransmitFrame;
-using obc::PingTelecommand;
 using telecommunication::uplink::IHandleTeleCommand;
-using telecommunication::downlink::DownlinkFrame;
-using telecommunication::downlink::DownlinkAPID;
 
 using namespace obc;
-
-void PingTelecommand::Handle(ITransmitFrame& transmitter, span<const uint8_t> parameters)
-{
-    UNREFERENCED_PARAMETER(parameters);
-
-    const char* response = "PONG";
-
-    DownlinkFrame frame(DownlinkAPID::Pong, 0);
-
-    frame.PayloadWriter().WriteArray(gsl::make_span(reinterpret_cast<const uint8_t*>(response), 4));
-
-    transmitter.SendFrame(frame.Frame());
-}
-
-uint8_t PingTelecommand::CommandCode() const
-{
-    return static_cast<uint8_t>('P');
-}
 
 Telecommands::Telecommands()
     : _ping(), //
---
+++ libs/obc/communication/telecommands/Include/obc/telecommands/ping.hpp
@@ -0,0 +1,24 @@
+#ifndef LIBS_OBC_COMMUNICATION_TELECOMMANDS_INCLUDE_OBC_TELECOMMANDS_PING_HPP_
+#define LIBS_OBC_COMMUNICATION_TELECOMMANDS_INCLUDE_OBC_TELECOMMANDS_PING_HPP_
+
+#include "telecommunication/telecommand_handling.h"
+
+namespace obc
+{
+    namespace telecommands
+    {
+        /**
+         * @brief Dummy ping telecommand that responds with pong
+         * @ingroup telecommands
+         */
+        class PingTelecommand final : public telecommunication::uplink::IHandleTeleCommand
+        {
+          public:
+            virtual void Handle(devices::comm::ITransmitFrame& transmitter, gsl::span<const std::uint8_t> parameters) override;
+
+            virtual std::uint8_t CommandCode() const override;
+        };
+    }
+}
+
+#endif /* LIBS_OBC_COMMUNICATION_TELECOMMANDS_INCLUDE_OBC_TELECOMMANDS_PING_HPP_ */
