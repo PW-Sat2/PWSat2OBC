0f58ef5ca449bb33e3860a987350293988a26fd2
Novakov <maciejt.nowak@gmail.com>
2017-02-13 17:29:03 +0100

    Respond with error frame if file is not found

--- integration_tests/tests/test_telecommands_fs.py
+++ integration_tests/tests/test_telecommands_fs.py
@@ -1,11 +1,13 @@
 import struct
 
 from devices import UplinkFrame
+from system import wait_for_obc_start
 from tests.base import BaseTest
 from utils import ensure_byte_list
 
 
 class FileSystemTelecommandsTest(BaseTest):
+    @wait_for_obc_start()
     def test_receive_multipart_file(self):
         data = ''.join(map(lambda x: x * 300, ['A', 'B', 'C']))
 
@@ -19,9 +21,9 @@ class FileSystemTelecommandsTest(BaseTest):
 
         frames = [
             self.system.comm.get_frame(20),
-            self.system.comm.get_frame(20),
-            self.system.comm.get_frame(20),
-            self.system.comm.get_frame(20)
+            self.system.comm.get_frame(1),
+            self.system.comm.get_frame(1),
+            self.system.comm.get_frame(1)
         ]
 
         frames = sorted(frames, key=lambda x: x.seq())
@@ -31,3 +33,17 @@ class FileSystemTelecommandsTest(BaseTest):
             received += ''.join([chr(b) for b in f.payload()])
 
         self.assertAlmostEqual(received, data)
+
+    @wait_for_obc_start()
+    def test_should_respond_with_error_frame_for_non_existent_file(self):
+        p = "/a/non_exist"
+
+        seqs = ensure_byte_list(struct.pack('<LLLL', 0, 3, 1, 2))
+
+        self.system.comm.put_frame(UplinkFrame(apid=0xAB, content=[0x11, len(p)] + list(p) + [0x0] + seqs))
+
+        frame = self.system.comm.get_frame(20)
+
+        self.assertEqual(frame.apid(), 2)
+        self.assertEqual(frame.seq(), 0)
+        self.assertEqual(frame.payload(), ensure_byte_list(p))
