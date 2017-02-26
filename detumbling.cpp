e3eeec6d8c408ce0aa3a6a55509fb6fb3f225c66
PetrerW <piotrw76@gmail.com>
2017-02-26 22:38:40 +0100

    Deleted isInitialised flag. Added units and desciption of DetumblingParameters

---
+++ detumbling.cpp
@@ -0,0 +1,128 @@
+/*
+ * detumbling.cpp
+ *
+ *  Created on: 25 Jan 2017
+ *      Author: weclewski
+ */
+
+/* MATLAB:
+ % ============================================================================
+ %
+ %                           detumbling procedure
+ %
+ % This procedure calculates the commanded magnetic dipole for the coils in
+ % the detumbling mode based on the B-Dot control law. A high-pass filter for
+ % the magnetic field time derivative is included.
+ % NOTE: Current magnetic field time derivative output (mtmDot) is the input
+ % in the next iteration (mtmDotPrev). Initialize the previous magnetic field
+ % time derivative with 0.
+ %
+ % Author: Pawel Jaworski
+ %         pawel.jaworski0@wp.pl
+ % Date:   december 2016
+ %
+ %   inputs        :
+ %       mtmMeas                  - [3x1], magnetometer measurement, [Gauss]
+ %       mtmMeasPrev              - [3x1], previous (one iteration back) magnetometer measurement, [Gauss]
+ %       mtmDotPrev               - [3x1], previous (one iteration back) magnetic field time derivative, [Gauss/s]
+ %
+ %   outputs       :
+ %       commDipoleBdot           - [3x1], commanded magnetic dipole, [A m^2]
+ %       mtmDot                   - [3x1], magnetic field time derivative, [Gauss/s]
+ %
+ %   globals       :
+ %       DetumblingConst.dt       - [1x1], iteration time step, [s]
+ %       DetumblingConst.wCutOff  - [1x1], high-pass filter cut off frequency, [rad/s]
+ %       DetumblingConst.bDotGain - [1x1], B-dot gain, [kg m^2 / s]
+ %       DetumblingConst.coilsOn  - boolean, [3x1], active magnetic coils
+ %
+ %   locals        :
+ %
+ %   subfunctions  :
+ %       vectorNorm               - calculates [3x1] vector norm
+ %
+ %   references    :
+ %
+ %
+ % ============================================================================
+ C:
+
+ Lsb of mtmMeas in int32 representation is 1e-9 [T] = 1e-5 [Gauss]
+ */
+
+#include "detumbling.hpp"
+#include <system.h>
+#include <cmath>
+
+using std::uint8_t;
+using std::uint16_t;
+using std::uint32_t;
+
+namespace adcs
+{
+
+Detumbling::Detumbling()
+{
+    //empty
+}
+
+void Detumbling::initializeDetumbling(DetumblingState& state,
+        const DetumblingParameters& param)
+{
+    UNUSED1(param);
+    //Set the previous time derivative of the magnetic field to zeros.
+    state.mtmDotPrev = RowVector3f::Zero();
+    //Set the previous MTM measurement to zeros,
+    state.mtmMeasPrev = RowVector3f::Zero(); // TODO on the first step should be initialised by measurement value
+}
+
+void Detumbling::stepDetumbling(DipoleVec& dipole, const MagVec& mgmt_meas,
+        DetumblingState& state,  const DetumblingParameters& param)///TODO units are wrong
+{
+    
+    
+        //conversion of input
+        std::array<float, 3> tmp;
+        std::copy(mgmt_meas.begin(), mgmt_meas.end(), tmp.begin());
+        RowVector3f mgmt_input(tmp.data());
+
+        // magnetic field time derivative
+        RowVector3f mtmDot = exp(-param.wCutOff * param.dt) * state.mtmDotPrev +
+                param.wCutOff * (mgmt_input - state.mtmMeasPrev);
+/*
+        RowVector3f mtmDot = state.mtmDotPrev;
+        mtmDot *= expf(-param.wCutOff * param.dt);
+        RowVector3f mtmDotDiff = mgmt_input - state.mtmMeasPrev;
+        mtmDotDiff *= param.wCutOff;
+        mtmDot += mtmDotDiff;
+*/
+        // commanded magnetic dipole to coils
+        RowVector3f commDipoleBdot = mtmDot * (-param.bDotGain) * 1e-4 / powf((mgmt_input* 1e-4).norm(), 2);
+/*
+        RowVector3f commDipoleBdot = mtmDot;
+        commDipoleBdot *= -param.bDotGain;
+        RowVector3f meas_tmp = mgmt_input;
+        meas_tmp *= 1e-4;
+        commDipoleBdot *= 1e-4 / powf(meas_tmp.norm(), 2);
+*/
+        if (!param.coilsOn[0]) // XXX Fixed: chaged `else if` to `if`
+            commDipoleBdot[0] = 0;
+        if (!param.coilsOn[1])
+            commDipoleBdot[1] = 0;
+        if (!param.coilsOn[2])
+            commDipoleBdot[2] = 0;
+
+        // store prev values
+        state.mtmDotPrev = mtmDot;
+        state.mtmMeasPrev = mgmt_input;
+
+        // convert to output
+        std::copy(commDipoleBdot.data(),
+                commDipoleBdot.data() + commDipoleBdot.size(), dipole.begin());
+    
+// XXX'0'?
+
+}
+
+}
+
---
+++ detumbling.hpp
@@ -0,0 +1,108 @@
+/*
+ * detumbling.hpp
+ *
+ *  Created on: 25 Jan 2017
+ *      Author: weclewski
+ */
+
+#ifndef ADCS_DETUMBLING_HPP_
+#define ADCS_DETUMBLING_HPP_
+
+#include "interface_types.hpp"
+#include <Eigen/Dense>
+#include <array>
+#include <cstdint>
+
+using std::uint8_t;
+using std::uint16_t;
+using std::uint32_t;
+using std::int32_t;
+using std::array;
+
+using Eigen::Matrix3d;
+using Eigen::Vector3d;
+using Eigen::RowVector3f;
+
+namespace adcs
+{
+/** @brief (Draft)   */
+class Detumbling
+final
+{
+    public:
+        /** @brief (Draft)   */
+       //using RowVector3i = Eigen::Matrix< int32_t , 1, 3 >;
+        //using RowVector3b = Eigen::Matrix< bool , 1, 3 >;
+
+        /** @brief (Draft)
+         * @unit:
+         */
+        static constexpr float DefaultDt = 0.2;
+        /** @brief (Draft)
+         * @unit: [s]
+         */
+        static constexpr float DefaultWCutOff = 0.2;
+        /** @brief (Draft)
+         * @unit: [rad/s]
+         */
+        static constexpr float DefaultBDotGain = 2.879285e-5; /// XXX unit conv
+        /** @brief (Draft)
+         * @unit: [kg*m^2/s]
+         */
+        static constexpr std::array <bool, 3> DefaultCoilsOn =
+        { true, true, true };
+
+        /** @brief (Draft)   */
+        struct DetumblingParameters //Parameters changeable by a telecommand from Earth. Those are constants used in detumbling.
+        {
+			/**
+			*	@change: if previous default values would give worse results in detumbling iterative algorithm than commanded from Earth (f.e. better convergence factor) 
+			**/
+            /** @brief (Draft)   */
+            float dt = DefaultDt; // iteration time step, [s]
+            /** @brief (Draft)   */
+            float wCutOff = DefaultWCutOff; // high-pass filter cut off frequency, [rad/s]
+            /** @brief (Draft)   */
+            float bDotGain = DefaultBDotGain; // B-dot gain, [kg m^2 / s] (contant k used to calculate commanded magnetic dipole) 
+											  //s.33 "Dropbox\PW-Sat2 udostepnione\documentation\documentation_phase_c\PW-Sat2-C-01.00-ADCS-CDR.pdf"
+            /** @brief (Draft)   */
+            std::array <bool, 3> coilsOn = DefaultCoilsOn; //active magnetic coils
+        };
+
+        /** @brief (Draft)   */
+        struct DetumblingState
+        {
+            /** @brief (Draft)   */
+            RowVector3f mtmDotPrev;
+            /** @brief (Draft)   */
+            RowVector3f mtmMeasPrev;
+            /** @brief (Draft)   */
+            //RowVector3f commDipoleBdot_table;
+        };
+
+        /** @brief (Draft)   */
+        Detumbling();
+
+        /**
+         * @brief (Draft)
+         * @param[out] state
+         * @param[in] parameters
+         * @return void
+         */
+        void initializeDetumbling(DetumblingState& state,
+                const DetumblingParameters& param);
+
+        /**
+         * @brief (Draft)
+         * @param[out] dipole
+         * @param[in] magnetometer
+         * @param[in/out] state
+         * @return void
+         */
+        void stepDetumbling(DipoleVec& dipole, const MagVec& mgmt,
+                DetumblingState& state, const DetumblingParameters& param);
+    };
+    }
+
+#endif /* ADCS_DETUMBLING_HPP_ */
+
