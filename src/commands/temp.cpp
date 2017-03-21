ac1cafadc1a41758bef98cd5580410cc917a9816
Novakov <maciejt.nowak@gmail.com>
2017-03-21 20:17:25 +0100

    Temperature sensor

---
+++ src/commands/temp.cpp
@@ -0,0 +1,15 @@
+#include <chrono>
+#include <cstdint>
+#include <em_adc.h>
+#include <em_cmu.h>
+#include <em_prs.h>
+#include "obc.h"
+
+using namespace std::chrono_literals;
+
+void Temp(std::uint16_t /*argc*/, char* /*argv*/ [])
+{
+    auto temp = Main.Hardware.MCUTemperature.ReadCelsius();
+
+    Main.terminal.Printf("Temp=%d\n", temp);
+}
