715ca1d9bb9afb5930d642e6096b0063244e4335
Novakov <maciejt.nowak@gmail.com>
2017-03-30 13:19:14 +0200

    Enabled IMTQ tests

--- integration_tests/tests/test_imtq.py
+++ integration_tests/tests/test_imtq.py
@@ -4,13 +4,13 @@ import unittest
 
 
 class Test_Imtq(BaseTest):
-    @unittest.skip("not working on current mock")
+    # @unittest.skip("not working on current mock")
     def test_bdot(self):
         self.assertEqual(self.system.imtq.mode.mode, Imtq.Mode.Type.idle)
         self.system.obc.bdot(2)
         self.assertEqual(self.system.imtq.mode.mode, Imtq.Mode.Type.detumble)
 
-    @unittest.skip("not working on current mock")
+    # @unittest.skip("not working on current mock")
     def test_read_magnetometer(self):
         self.system.imtq.mtm_measurement = [100, 200, -300]
         res = self.system.obc.read_magnetometer()
