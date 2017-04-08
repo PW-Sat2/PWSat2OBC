c385d4ccd769599e7bbdde60143705f047341748
Łukasz Żak <zaklukasz32@gmail.com>
2017-04-08 23:24:18 +0200

    [time] Extend, update persistent state & time synchronization documentation.

--- libs/mission/time/Include/mission/time.hpp
+++ libs/mission/time/Include/mission/time.hpp
@@ -3,12 +3,11 @@
 
 #pragma once
 
+#include <tuple>
 #include "mission/base.hpp"
 #include "rtc/rtc.hpp"
 #include "state/struct.h"
 #include "time/timer.h"
-
-#include <tuple>
 
 using services::time::TimeProvider;
 
@@ -23,7 +22,43 @@ namespace mission
      */
 
     /**
-     * @brief Task that is responsible for updating current state in global mission state object and correct time based on external RTC.
+     * @brief Task that is responsible for updating current state in global mission state object
+     * and correct time based on external RTC.
+     *
+     * The time synchronization process is run once every 15 minutes assuming that synchronization process succeeds.
+     * The time synchronization process will adjust current mission time. The process takes as its input:
+     * - current mission time: T<sub>mcu</sub>
+     * - current external time: T<sub>rtc</sub>
+     * - mission time from last synchronization: T<sub>mcu<sub>n - 1</sub></sub>
+     * - external time from last synchronization: T<sub>rtc<sub>n - 1</sub></sub>
+     * - internal clock synchronization factor: w<sub>mcu</sub>
+     * - external clock synchronization factor: w<sub>rtc</sub>
+     *
+     * The adjustment process is as follows:
+     *
+     * delta<sub>mcu</sub> = T<sub>mcu</sub> - T<sub>mcu<sub>n - 1</sub></sub>
+     *
+     * delta<sub>rtc</sub> = T<sub>rtc</sub> - T<sub>rtc<sub>n - 1</sub></sub>
+     *
+     * Assuming that time should always monotonically increase, any time increment
+     * (either delta<sub>mcu</sub>, delta<sub>rtc</sub> or both) that is negative is set to zero,
+     * therefore the negative increments are treated as temporary anomalies not a rule. As a result
+     * if for some reason one of the time sources will report decreasing time its increment
+     * will be ignored and corrected so it will not pose any problems in future. The current mission time
+     * is calculated using the following formula:
+     *
+     * T<sub>mcu<sub>n</sub></sub> = T<sub>mcu<sub>n - 1</sub></sub> + (delta<sub>mcu</sub> * w<sub>mcu</sub> + delta<sub>rtc</sub> *
+     * w<sub>rtc</sub>) / (w<sub>mcu</sub> + w<sub>rtc</sub>)
+     *
+     * T<sub>rtc<sub>n</sub></sub> = T<sub>rtc</sub>
+     *
+     * In order to prevent long jumps in time caused by (unlikely) random bit flips the time correction value
+     * calculated as abs(T<sub>mcu<sub>n</sub></sub> - T<sub>mcu</sub>) cannot be greater than two hours.
+     * As the time synchronization should be done once in 15 minutes then the most likely time correction
+     * value should be at most in several seconds at most. Under these conditions, in normal circumstances the
+     * time correction value should never reach level of hours. If it does then it is more likely result
+     * of random error or external interference then the actual time synchronization error.
+     * In such case the time adjustment process is ignored and current mission/external time captured for future.
      */
     class TimeTask : public Update, public Action
     {
@@ -64,15 +99,20 @@ namespace mission
          */
         ActionDescriptor<SystemState> BuildAction();
 
+        /**
+         * @brief This procedure performs actual time correction/synchronization.
+         *
+         * @param[in] missionTime Currently observed mission time.
+         * @param[in] externalTime Currently observed externally measured time.
+         * @param[in] synchronizationState Previous synchronization state.
+         * @param[in] correctionConfiguation Time synchronization configuration object.
+         * @return Corrected mission time.
+         */
         static std::chrono::milliseconds PerformTimeCorrection(std::chrono::milliseconds missionTime, //
             std::chrono::milliseconds externalTime,                                                   //
             const state::TimeState& synchronizationState,                                             //
             const state::TimeCorrectionConfiguration& correctionConfiguation                          //
             );
-
-        UpdateResult UpdateSatelliteTime(SystemState& state);
-
-        void UpdatePersistentState(state::SystemPersistentState& state);
 
       private:
         /**
@@ -97,7 +137,12 @@ namespace mission
          */
         static void CorrectTimeProxy(const SystemState& state, void* param);
 
+        /**
+         * @brief Time correction procedure, that will correct current time based on the external RTC.
+         * @param[in] state Reference to global mission state.
+         */
         void CorrectTime(const SystemState& state);
+
         /**
          * @brief Time provider reference.
          */
