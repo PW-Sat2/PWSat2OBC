4f7bb9a363ed3ed7fe798ec4c32c546c7b4e238a
Łukasz Żak <zaklukasz32@gmail.com>
2017-05-02 21:26:05 +0200

    [mission][telemetry] Documentation for telemetry saving task.

--- libs/mission/telemetry/Include/mission/telemetry.hpp
+++ libs/mission/telemetry/Include/mission/telemetry.hpp
@@ -16,65 +16,111 @@ namespace mission
      * @defgroup mission_telemetry Telemetry serialization
      * @ingroup mission
      *
-     * Module that saves current telemetry state to files.
+     * @brief Module that saves current telemetry state to files.
      * @{
      */
 
+    /**
+     * @brief This type contains configuration of telemetry saving task.
+     */
     struct TelemetryConfiguration
     {
+        /**
+         * @brief Path to current telemetry event file.
+         */
         const char* currentFileName;
+
+        /**
+         * @brief Path to previous telemetry event file.
+         */
         const char* previousFileName;
+
+        /**
+         * @brief Maximal size of single telemetry event file.
+         */
         std::int32_t maxFileSize;
     };
+
     /**
-     * @brief Task that is responsible for updating current state in global mission state object
-     * and correct time based on external RTC.
+     * @brief This task is responsible for observing the telemetry container state and as soon
+     * as change is observed extract save it to telemetry event file.
+     *
+     * This task uses two files for saving the state of the telemetry changes:
+     * - current telemetry file - This file is actively used and contains the most recent telemetry changes.
+     * - previous telemetry file - This is archival file that contains telemetry from the
+     * longer period of time that immediately precedes the time period covered by the
+     * \a current \a telemetry \a file
+     *
+     * The newest changes to the telemetry elements are being added to the end of the
+     * \a current \a telemetry \a file. Once the current telemetry file reaches configured size
+     * it is archived.
+     *
+     * The telemetry archivization process is done by removing \a previous \a telemetry \a file and
+     * changing \a current \a telemetry \a file to namd of the \a previous \a telemetry \a file.
      */
     class TelemetryTask : public Action
     {
       public:
         /**
          * @brief ctor.
-         * @param[in] arguments Reference to time providier argument list.
+         * @param[in] arguments Reference to file system provider & current task configuration.
          */
         TelemetryTask(std::tuple<services::fs::IFileSystem&, TelemetryConfiguration> arguments);
 
         /**
          * @brief Builds action descriptor for this task.
-         * @return Action descriptor - the time correction task.
+         * @return Action descriptor - the telemetry change save task.
          */
         ActionDescriptor<SystemState> BuildAction();
 
         /**
-         * @brief Time correction action, that will correct current time based on the external RTC.
+         * @brief This procedure extracts modified parts of the telemetry and saves it to the telemetry
+         * event file.
+         *
+         * Once the process is complete the telemetry container is notified that all changes are saved.
          * @param[in] state Reference to global mission state.
          */
         void Save(const SystemState& state);
 
+        /**
+         * @brief This procedure is responsible for appending the passed data frame to the current
+         * telemetry event file.
+         *
+         * @param[in] frame Buffer with data frame that should be added to file.
+         * @return Operation status, true on success, false otherwise.
+         */
         bool SaveToFile(gsl::span<const std::uint8_t> frame);
 
       private:
         /**
-         * @brief Condition for time correction action.
+         * @brief Condition for telemetry saving action.
          * @param[in] state Reference to global mission state.
          * @param[in] param Current execution context.
-         * @return true if correction action should be executed, false otherwise.
+         * @return true if there are telemetry changes that should be saved, false otherwise.
          */
         static bool SaveCondition(const SystemState& state, void* param);
 
         /**
-         * @brief Time correction action, that will correct current time based on the external RTC.
+         * @brief This procedure is responsible for appending the passed data frame to the current
+         * telemetry event file.
+         *
          * @param[in] state Reference to global mission state.
          * @param[in] param Current execution context.
          */
         static void SaveProxy(const SystemState& state, void* param);
 
+        /**
+         * @brief File system provider.
+         */
         services::fs::IFileSystem& provider;
 
+        /**
+         * @bier Current configuration
+         */
         TelemetryConfiguration configuration;
     };
 
     /** @} */
 }
 
-#endif /* LIBS_MISSION_INCLUDE_MISSION_TIME_HPP_ */
+#endif /* LIBS_MISSION_INCLUDE_MISSION_TELEMETRY_HPP_ */
