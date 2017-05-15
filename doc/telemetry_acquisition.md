638ad2d8acea69f7363ecc0bcb9d380afdd5464e
Łukasz Żak <zaklukasz32@gmail.com>
2017-05-15 12:20:40 +0200

    [telemetry] Update telemetry acquisition documentation.
    Additionally add dedicated page that contains current list of telemetry acquisition tasks.

---
+++ doc/telemetry_acquisition.md
@@ -0,0 +1,3 @@
+# Telemetry Acquisition {#telemetry_acquisition_list}
+
+List below shows all available tasks that are resposnible for acquiring live telemetry, please refer to specific documentation pages for details.
--- libs/telemetry/comm/Include/telemetry/collect_comm.hpp
+++ libs/telemetry/comm/Include/telemetry/collect_comm.hpp
@@ -9,23 +9,44 @@
 
 namespace telemetry
 {
+    /**
+     * @brief This task is responsible for acquiring & updating comm hardware telemetry.
+     * @telemetry_acquisition
+     * @ingroup telemetry
+     */
     class CommTelemetryAcquisition : public mission::Update
     {
       public:
+        /**
+         * @brief ctor.
+         * @param[in] comm Reference to comm driver that will provide this module with hardware telemetry
+         */
         CommTelemetryAcquisition(devices::comm::ICommTelemetryProvider& comm);
 
+        /**
+         * @brief Builds update descriptor for this task.
+         * @return Update descriptor - the comm telemetry acquisition update task.
+         */
         mission::UpdateDescriptor<telemetry::TelemetryState> BuildUpdate();
 
+        /**
+         * @brief Acquires comm telemetry & stores it in passed state object.
+         * @param[in] state Object that should be updated with new comm hardware telemetry.
+         * @return Telemetry acquisition result.
+         */
         mission::UpdateResult UpdateCommTelemetry(telemetry::TelemetryState& state);
 
       private:
         /**
-         * @brief Updates current time in global mission state.
-         * @param[in] state Reference to global mission state.
+         * @brief Updates current comm telemetry in global state.
+         * @param[in] state Reference to global state.
          * @param[in] param Current execution context.
          */
         static mission::UpdateResult UpdateProc(telemetry::TelemetryState& state, void* param);
 
+        /**
+         * @brief Reference to comm driver.
+         */
         devices::comm::ICommTelemetryProvider* provider;
     };
 }
