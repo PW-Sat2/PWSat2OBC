a90da1161680a84fbb19d3e309c6506f2c56add6
Marek Antoniak <kfazol@gmail.com>
2017-05-18 13:23:10 +0200

    [ADCS] Implemented built-in detumbling

--- libs/adcs/builtin/BuiltinDetumbling.cpp
+++ libs/adcs/builtin/BuiltinDetumbling.cpp
@@ -1,7 +1,17 @@
 #include "BuiltinDetumbling.hpp"
+#include <chrono>
 
 namespace adcs
 {
+    BuiltinDetumbling::BuiltinDetumbling(devices::imtq::IImtqDriver& imtqDriver_) : imtqDriver(imtqDriver_)
+    {
+    }
+
+    OSResult BuiltinDetumbling::Initialize()
+    {
+        return OSResult::Success;
+    }
+
     OSResult BuiltinDetumbling::Enable()
     {
         return OSResult::Success;
@@ -9,15 +19,16 @@ namespace adcs
 
     OSResult BuiltinDetumbling::Disable()
     {
-        return OSResult::Success;
+        return imtqDriver.CancelOperation() ? OSResult::Success : OSResult::IOError;
     }
 
     void BuiltinDetumbling::Process()
     {
+        imtqDriver.StartBDotDetumbling(chrono_extensions::period_cast<std::chrono::seconds>(BuiltinDetumbling::Frequency));
     }
 
     std::chrono::milliseconds BuiltinDetumbling::GetWait() const
     {
-        return std::chrono::milliseconds{1};
+        return chrono_extensions::period_cast<std::chrono::milliseconds>(BuiltinDetumbling::Frequency);
     }
 }
--- libs/adcs/builtin/Include/adcs/BuiltinDetumbling.hpp
+++ libs/adcs/builtin/Include/adcs/BuiltinDetumbling.hpp
@@ -4,6 +4,8 @@
 #pragma once
 
 #include "adcs/adcs.hpp"
+#include "base/hertz.hpp"
+#include "imtq/imtq.h"
 
 namespace adcs
 {
@@ -14,6 +16,15 @@ namespace adcs
     class BuiltinDetumbling final : public IAdcsProcessor
     {
       public:
+        /**
+         * @brief Ctor.
+         *
+         * @param[in] imtqDriver_ Low level imtq module driver.
+         */
+        BuiltinDetumbling(devices::imtq::IImtqDriver& imtqDriver_);
+
+        virtual OSResult Initialize() final override;
+
         virtual OSResult Enable() final override;
 
         virtual OSResult Disable() final override;
@@ -22,8 +33,11 @@ namespace adcs
 
         virtual std::chrono::milliseconds GetWait() const override final;
 
+        /** @brief Algorithm refresh frequency. */
+        static constexpr chrono_extensions::hertz Frequency = chrono_extensions::hertz{0.03};
+
       private:
-        // TODO integrate imtq here
+        devices::imtq::IImtqDriver& imtqDriver;
     };
 }
 
