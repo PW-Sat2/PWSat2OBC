1f4f63d6f995bb999255b83266e40b0477621052
Marek Antoniak <kfazol@gmail.com>
2017-05-18 17:06:05 +0200

    [TeleComm] Added SetTimeCorrectionConfig telecommand

---
+++ libs/base/Include/base/IHasState.hpp
@@ -0,0 +1,30 @@
+#ifndef LIBS_MISSION_INCLUDE_MISSION_IHASSTATE_H_
+#define LIBS_MISSION_INCLUDE_MISSION_IHASSTATE_H_
+
+#include <type_traits>
+
+/**
+ * @brief Interface marking object as stateful.
+ * @ingroup StateDef
+ */
+template <typename State> struct IHasState
+{
+    /**
+     * @brief Type of the state object.
+     */
+    typedef typename std::remove_reference<State>::type StateType;
+
+    /**
+     * @brief State accessor.
+     * @return Reference to the state.
+     */
+    virtual const StateType& GetState() const noexcept = 0;
+
+    /**
+     * @brief State accessor.
+     * @return Reference to the state.
+     */
+    virtual StateType& GetState() noexcept = 0;
+};
+
+#endif /* LIBS_MISSION_INCLUDE_MISSION_IHASSTATE_H_ */
