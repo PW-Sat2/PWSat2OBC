334953e8cf8643df896498e7627b4364e9759434
Novakov <maciejt.nowak@gmail.com>
2017-05-21 14:29:58 +0200

    Kick internal watchdog in tick notification

--- integration_tests/tests/test_watchdog.py
+++ integration_tests/tests/test_watchdog.py
@@ -8,7 +8,7 @@ from tests.base import BaseTest
 class WatchdogTest(BaseTest):
     def test_should_restart_when_mcu_hangs(self):
         self.system.obc.hang()
-        rebooted = self.system.obc.wait_for_boot(3)
+        rebooted = self.system.obc.wait_for_boot(70)
         logging.info("flag=%s", str(rebooted))
         if not rebooted:
             self.system.restart()  # allow clean test tear down
