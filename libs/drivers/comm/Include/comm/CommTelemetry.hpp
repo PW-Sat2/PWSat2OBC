652213d91c6627d9f349007a3530da316907aa8a
Łukasz Żak <zaklukasz32@gmail.com>
2017-05-29 21:49:48 +0200

    [telemetry][comm] Unit tests for comm telemetry object.

--- libs/drivers/comm/Include/comm/CommTelemetry.hpp
+++ libs/drivers/comm/Include/comm/CommTelemetry.hpp
@@ -51,6 +51,84 @@ class CommTelemetry
      */
     static constexpr std::uint32_t BitSize();
 
+    /**
+     * @brief Returns raw measurement value of the transmitter current consumption.
+     * @return Raw measurement value of the transmitter current consumption.
+     */
+    uint12_t TransmitterCurrentConsumption() const noexcept;
+
+    /**
+     * @brief Returns raw measurement value of the receiver current consumption.
+     * @return Raw measurement value of the receiver current consumption.
+     */
+    uint12_t ReceiverCurrentConsumption() const noexcept;
+
+    /**
+     * @brief Returns Raw measurement value of the instantaneous Doppler offset.
+     * @return Raw measurement value of the instantaneous Doppler offset of the signal at the receiver port.
+     */
+    uint12_t DopplerOffset() const noexcept;
+
+    /**
+     * @brief Returns raw measurement value of the power bus voltage.
+     * @return Raw measurement value of the power bus voltage.
+     */
+    uint12_t Vcc() const noexcept;
+
+    /**
+     * @brief Returns Raw measurement value of the local oscillator temperature.
+     * @return Raw measurement value of the local oscillator temperature.
+     */
+    uint12_t OscilatorTemperature() const noexcept;
+
+    /**
+     * @brief Returns raw measurement value of the power amplifier temperature.
+     * @return Raw measurement value of the power amplifier temperature.
+     */
+    uint12_t ReceiverAmplifierTemperature() const noexcept;
+
+    /**
+     * @brief Returns raw measurement value of the instantaneous signal strength of the signal at the receiver.
+     * @return Raw measurement value of the instantaneous signal strength of the signal at the receiver.
+     */
+    uint12_t SignalStrength() const noexcept;
+
+    /**
+     * @brief Returns raw measurement value of the instantaneous RF reflected power at the transmitter port.
+     * @return Raw measurement value of the instantaneous RF reflected power at the transmitter port.
+     */
+    uint12_t RFReflectedPower() const noexcept;
+
+    /**
+     * @brief Returns raw measurement value of the power amplifier temperature.
+     * @return Raw measurement value of the power amplifier temperature.
+     */
+    uint12_t TransmitterAmplifierTemperature() const noexcept;
+
+    /**
+     * @brief Returns raw measurement value of the instantaneous RF forward power at the transmitter port.
+     * @return Raw measurement value of the instantaneous RF forward power at the transmitter port.
+     */
+    uint12_t RFForwardPower() const noexcept;
+
+    /**
+     * @brief Returns object containing transmitter mcu uptime.
+     * @return Transmitter mcu uptime.
+     */
+    const Uptime& TransmitterUptime() const noexcept;
+
+    /**
+     * @brief Returns information whether transmitter carrier is currently being send.
+     * @return True in case transmitter carrier is currently being send, false otherwise.
+     */
+    bool TransmitterIdleState() const noexcept;
+
+    /**
+     * @brief Returns information whether beacon is active.
+     * @return True in case beacon is currently active, false otherwise.
+     */
+    bool BeaconState() const noexcept;
+
   private:
     /** @brief Raw measurement value of the transmitter current consumption. */
     uint12_t transmitterCurrentConsumption;
@@ -97,6 +175,71 @@ constexpr std::uint32_t CommTelemetry::BitSize()
         decltype(Uptime::hours)::Size +   //
         decltype(Uptime::days)::Size +    //
         2;
+}
+
+inline uint12_t CommTelemetry::TransmitterCurrentConsumption() const noexcept
+{
+    return this->transmitterCurrentConsumption;
+}
+
+inline uint12_t CommTelemetry::ReceiverCurrentConsumption() const noexcept
+{
+    return this->receiverCurrentConsumption;
+}
+
+inline uint12_t CommTelemetry::DopplerOffset() const noexcept
+{
+    return this->dopplerOffset;
+}
+
+inline uint12_t CommTelemetry::Vcc() const noexcept
+{
+    return this->vcc;
+}
+
+inline uint12_t CommTelemetry::OscilatorTemperature() const noexcept
+{
+    return this->oscilatorTemperature;
+}
+
+inline uint12_t CommTelemetry::ReceiverAmplifierTemperature() const noexcept
+{
+    return this->receiverAmplifierTemperature;
+}
+
+inline uint12_t CommTelemetry::SignalStrength() const noexcept
+{
+    return this->signalStrength;
+}
+
+inline uint12_t CommTelemetry::RFReflectedPower() const noexcept
+{
+    return this->rFReflectedPower;
+}
+
+inline uint12_t CommTelemetry::TransmitterAmplifierTemperature() const noexcept
+{
+    return this->transmitterAmplifierTemperature;
+}
+
+inline uint12_t CommTelemetry::RFForwardPower() const noexcept
+{
+    return this->rFForwardPower;
+}
+
+inline const Uptime& CommTelemetry::TransmitterUptime() const noexcept
+{
+    return this->transmitterUptime;
+}
+
+inline bool CommTelemetry::TransmitterIdleState() const noexcept
+{
+    return this->transmitterIdleState;
+}
+
+inline bool CommTelemetry::BeaconState() const noexcept
+{
+    return this->beaconState;
 }
 
 COMM_END
