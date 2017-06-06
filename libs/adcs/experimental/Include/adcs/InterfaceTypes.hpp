df1c23481512d892c45b277f2db89121041559cf
pweclewski <weclewski.piotr@gmail.com>
2017-06-06 01:56:33 +0100

    implemetation of sunpointing icd and necessary interfaces types, adding sunpointing to the project

--- libs/adcs/experimental/Include/adcs/InterfaceTypes.hpp
+++ libs/adcs/experimental/Include/adcs/InterfaceTypes.hpp
@@ -12,21 +12,37 @@ namespace adcs
      * @{
      */
 
-    /** @brief (Draft) Dipole representaiton
+    /** @brief (Draft) Dipole representation
      *          1 LSB = 1e-4 Am^2
      */
     using Dipole = std::int16_t;
 
-    /** @brief (Draft) Dipole Vector (3d) representaiton  */
+    /** @brief Dipole Vector (3d) representation  */
     using DipoleVec = std::array<Dipole, 3>;
 
-    /** @brief (Draft) Magnetometer Measurement representaiton
+    /** @brief Magnetometer Measurement representation
      *          1 LSB = 1e-7 T
      */
     using MagnetometerMeasurement = std::int32_t;
 
-    /** @brief (Draft) Magnetometer Measurement Vector (3d) representaiton */
+    /** @brief Magnetometer Measurement Vector (3d) representation */
     using MagVec = std::array<MagnetometerMeasurement, 3>;
+
+    /** @brief Gyroscope calibrated Measurement representation
+     *          [rad/s]
+     */
+    using GyroscopeMeasurement = float;
+
+    /** @brief Gyroscope Measurements Vector (3 axes) */
+    using GyroVec = std::array<GyroscopeMeasurement, 3>;
+
+    /** @brief Sun Sensor Measurement representation
+     *          [rad]
+      */
+    using SunSensorMeasurement = float;
+
+    /** @brief (Draft) Sun Sensor processed Measurement Vector (2 angles) */
+    using SunsVec = std::array<SunSensorMeasurement, 2>;
 
     /** @} */
 }
