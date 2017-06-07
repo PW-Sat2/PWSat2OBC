8ec3fba0764eb37bfd4bc7aad305c40cec0b7712
Novakov <maciejt.nowak@gmail.com>
2017-06-07 18:03:56 +0200

    Set boot slots telecommand

--- src/obc.cpp
+++ src/obc.cpp
@@ -55,14 +55,22 @@ static void TimePassed(void* /*context*/, TimePoint /*currentTime*/)
 }
 
 OBC::OBC()
-    : BootTable(Hardware.FlashDriver),                                                                               //
-      BootSettings(this->Hardware.PersistentStorage.GetRedundantDriver()),                                           //
-      Hardware(this->Fdir.ErrorCounting(), this->PowerControlInterface, timeProvider),                               //
-      PowerControlInterface(this->Hardware.EPS),                                                                     //
-      Storage(this->Fdir.ErrorCounting(), Hardware.SPI, fs, Hardware.Pins),                                          //
-      Experiments(fs, this->adcs.GetAdcsController(), this->timeProvider),                                           //
-      Communication(this->Fdir, this->Hardware.CommDriver, this->timeProvider, Mission, fs, Experiments, BootTable), //
-      terminal(this->GetLineIO())                                                                                    //
+    : BootTable(Hardware.FlashDriver),                                                 //
+      BootSettings(this->Hardware.PersistentStorage.GetRedundantDriver()),             //
+      Hardware(this->Fdir.ErrorCounting(), this->PowerControlInterface, timeProvider), //
+      PowerControlInterface(this->Hardware.EPS),                                       //
+      Storage(this->Fdir.ErrorCounting(), Hardware.SPI, fs, Hardware.Pins),                                        //
+      Experiments(fs, this->adcs.GetAdcsController(), this->timeProvider),             //
+      Communication(                                                                   //
+          this->Fdir,
+          this->Hardware.CommDriver,
+          this->timeProvider,
+          Mission,
+          fs,
+          Experiments,
+          BootTable,
+          BootSettings),          //
+      terminal(this->GetLineIO()) //
 {
 }
 
