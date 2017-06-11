1ea4e7636e95133f5e2b2395683c581beb0e9a44
Novakov <maciejt.nowak@gmail.com>
2017-06-11 21:54:14 +0200

    Majority voting on 5 elements

--- libs/base/Include/redundancy.hpp
+++ libs/base/Include/redundancy.hpp
@@ -43,6 +43,13 @@ namespace redundancy
         return (a & b) | (b & c) | (a & c);
     }
 
+    template <typename T> T Correct(T a, T b, T c, T d, T e)
+    {
+        static_assert(std::is_integral<T>::value || std::is_enum<T>::value, "Correction requires integral or enum type");
+        return (c & d & e) | (b & d & e) | (b & c & e) | (b & c & d) | (a & d & e) | (a & c & e) | (a & c & d) | (a & b & e) | (a & b & d) |
+            (a & b & c);
+    }
+
     /**
      * @brief Performs bitwise majority vote based on three elements in array.
      * @param[in] elements Array with three elements
@@ -71,6 +78,22 @@ namespace redundancy
         for (auto i = 0; i < buffer1.length(); ++i)
         {
             buffer1[i] = Correct(buffer1[i], buffer2[i], buffer3[i]);
+        }
+
+        return true;
+    }
+
+    template <typename T> bool CorrectBuffer(gsl::span<T> result, const std::array<gsl::span<T>, 5>& buffers)
+    {
+        static_assert(std::is_integral<T>::value || std::is_enum<T>::value, "Correction requires integral or enum type");
+        if (buffers[0].length() != buffers[1].length() || buffers[1].length() != buffers[2].length() ||
+            buffers[2].length() != buffers[3].length() || buffers[3].length() != buffers[4].length() ||
+            result.length() != buffers[0].length())
+            return false;
+
+        for (auto i = 0; i < result.length(); ++i)
+        {
+            result[i] = Correct(buffers[0][i], buffers[1][i], buffers[2][i], buffers[3][i], buffers[4][i]);
         }
 
         return true;
--- unit_tests/others/base/RedundancyTest.cpp
+++ unit_tests/others/base/RedundancyTest.cpp
@@ -7,6 +7,7 @@
 
 using testing::Test;
 using testing::Eq;
+using testing::ElementsAreArray;
 
 using std::uint8_t;
 using std::uint32_t;
@@ -14,11 +15,7 @@ using gsl::span;
 
 using namespace redundancy;
 
-class RedundancyTest : public Test
-{
-};
-
-TEST_F(RedundancyTest, ShouldFixSingleByte)
+TEST(RedundancyTest3, ShouldFixSingleByte)
 {
     ASSERT_THAT(Correct(0b0000000, 0b0000000, 0b0000000), Eq(0b0000000));
     ASSERT_THAT(Correct(0b0000001, 0b0000000, 0b0000000), Eq(0b0000000));
@@ -35,7 +32,7 @@ TEST_F(RedundancyTest, ShouldFixSingleByte)
     ASSERT_THAT(Correct(0b1010101, 0b1110000, 0b0010001), Eq(0b1010001));
 }
 
-TEST_F(RedundancyTest, ShouldAllBytesInArray)
+TEST(RedundancyTest3, ShouldAllBytesInArray)
 {
     uint8_t array1[] = {0b10101010, 0b1010101, 0b1010101};
     uint8_t array2[] = {0b11001100, 0b1110000, 0b1110000};
@@ -51,7 +48,7 @@ TEST_F(RedundancyTest, ShouldAllBytesInArray)
     ASSERT_THAT(b1, Eq(gsl::span<uint8_t>(expect)));
 }
 
-TEST_F(RedundancyTest, Voter)
+TEST(RedundancyTest3, Voter)
 {
     ASSERT_THAT(Vote<uint8_t>(1, 1, 1), Eq(Some<uint8_t>(1)));
     ASSERT_THAT(Vote<uint8_t>(1, 1, 2), Eq(Some<uint8_t>(1)));
@@ -59,4 +56,60 @@ TEST_F(RedundancyTest, Voter)
     ASSERT_THAT(Vote<uint8_t>(2, 1, 1), Eq(Some<uint8_t>(1)));
     ASSERT_THAT(Vote<uint8_t>(1, 2, 2), Eq(Some<uint8_t>(2)));
     ASSERT_THAT(Vote<uint8_t>(1, 2, 3), Eq(None<uint8_t>()));
+}
+
+TEST(RedundancyTest5, ShouldFixSingleByte)
+{
+    ASSERT_THAT(Correct(0b0, 0b0, 0b0, 0b0, 0b0), Eq(0));
+    ASSERT_THAT(Correct(0b0, 0b0, 0b0, 0b0, 0b1), Eq(0));
+    ASSERT_THAT(Correct(0b0, 0b0, 0b0, 0b1, 0b0), Eq(0));
+    ASSERT_THAT(Correct(0b0, 0b0, 0b0, 0b1, 0b1), Eq(0));
+    ASSERT_THAT(Correct(0b0, 0b0, 0b1, 0b0, 0b0), Eq(0));
+    ASSERT_THAT(Correct(0b0, 0b0, 0b1, 0b0, 0b1), Eq(0));
+    ASSERT_THAT(Correct(0b0, 0b0, 0b1, 0b1, 0b0), Eq(0));
+    ASSERT_THAT(Correct(0b0, 0b0, 0b1, 0b1, 0b1), Eq(1));
+    ASSERT_THAT(Correct(0b0, 0b1, 0b0, 0b0, 0b0), Eq(0));
+    ASSERT_THAT(Correct(0b0, 0b1, 0b0, 0b0, 0b1), Eq(0));
+    ASSERT_THAT(Correct(0b0, 0b1, 0b0, 0b1, 0b0), Eq(0));
+    ASSERT_THAT(Correct(0b0, 0b1, 0b0, 0b1, 0b1), Eq(1));
+    ASSERT_THAT(Correct(0b0, 0b1, 0b1, 0b0, 0b0), Eq(0));
+    ASSERT_THAT(Correct(0b0, 0b1, 0b1, 0b0, 0b1), Eq(1));
+    ASSERT_THAT(Correct(0b0, 0b1, 0b1, 0b1, 0b0), Eq(1));
+    ASSERT_THAT(Correct(0b0, 0b1, 0b1, 0b1, 0b1), Eq(1));
+    ASSERT_THAT(Correct(0b1, 0b0, 0b0, 0b0, 0b0), Eq(0));
+    ASSERT_THAT(Correct(0b1, 0b0, 0b0, 0b0, 0b1), Eq(0));
+    ASSERT_THAT(Correct(0b1, 0b0, 0b0, 0b1, 0b0), Eq(0));
+    ASSERT_THAT(Correct(0b1, 0b0, 0b0, 0b1, 0b1), Eq(1));
+    ASSERT_THAT(Correct(0b1, 0b0, 0b1, 0b0, 0b0), Eq(0));
+    ASSERT_THAT(Correct(0b1, 0b0, 0b1, 0b0, 0b1), Eq(1));
+    ASSERT_THAT(Correct(0b1, 0b0, 0b1, 0b1, 0b0), Eq(1));
+    ASSERT_THAT(Correct(0b1, 0b0, 0b1, 0b1, 0b1), Eq(1));
+    ASSERT_THAT(Correct(0b1, 0b1, 0b0, 0b0, 0b0), Eq(0));
+    ASSERT_THAT(Correct(0b1, 0b1, 0b0, 0b0, 0b1), Eq(1));
+    ASSERT_THAT(Correct(0b1, 0b1, 0b0, 0b1, 0b0), Eq(1));
+    ASSERT_THAT(Correct(0b1, 0b1, 0b0, 0b1, 0b1), Eq(1));
+    ASSERT_THAT(Correct(0b1, 0b1, 0b1, 0b0, 0b0), Eq(1));
+    ASSERT_THAT(Correct(0b1, 0b1, 0b1, 0b0, 0b1), Eq(1));
+    ASSERT_THAT(Correct(0b1, 0b1, 0b1, 0b1, 0b0), Eq(1));
+    ASSERT_THAT(Correct(0b1, 0b1, 0b1, 0b1, 0b1), Eq(1));
+}
+
+TEST(RedundancyTest5, ShouldAllBytesInArray)
+{
+    std::array<uint8_t, 3> array1{0b10101010, 0b01010101, 0b11010101};
+    std::array<uint8_t, 3> array2{0b11001100, 0b01110000, 0b01110000};
+    std::array<uint8_t, 3> array3{0b11110000, 0b00010001, 0b10010001};
+    std::array<uint8_t, 3> array4{0b00100000, 0b00111101, 0b10110011};
+    std::array<uint8_t, 3> array5{0b10110010, 0b11001110, 0b11110000};
+
+    uint8_t expect[] = {0b10100000, 0b01010101, 0b11110001};
+
+    std::array<gsl::span<uint8_t>, 5> spans{array1, array2, array3, array4, array5};
+
+    std::array<uint8_t, 3> result{0};
+
+    auto r = CorrectBuffer(gsl::make_span(result), spans);
+
+    ASSERT_THAT(r, Eq(true));
+    ASSERT_THAT(result, ElementsAreArray(expect, 3));
 }
