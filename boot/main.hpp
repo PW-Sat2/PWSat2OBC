141abc0c8c4e915c1a87a5ec88b875ef5a10ac12
Łukasz Żak <zaklukasz32@gmail.com>
2017-07-07 18:02:56 +0200

    [platform] Update boot loader & safe mode application to match recent platform changes.

--- boot/main.hpp
+++ boot/main.hpp
@@ -6,7 +6,7 @@
 #include "error_counter.hpp"
 #include "flash_driver.hpp"
 #include "fm25w/fm25w.hpp"
-#include "io_map.h"
+#include "mcu/io_map.h"
 #include "program_flash/boot_table.hpp"
 #include "spi.hpp"
 
