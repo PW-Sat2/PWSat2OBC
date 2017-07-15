181dee0aa1fadf8dfdb923c785e4934aeafaa2b7
Novakov <maciejt.nowak@gmail.com>
2017-07-15 17:59:44 +0200

    Terminal command for reporting current error counters status

--- src/commands/error_counters.cpp
+++ src/commands/error_counters.cpp
@@ -1,14 +1,36 @@
 #include <cstdint>
+#include <cstring>
 #include "obc/fdir.hpp"
 #include "obc_access.hpp"
 #include "terminal/terminal.h"
 
-void ErrorCountersCommand(std::uint16_t /*argc*/, char* /*argv*/ [])
+static void ShowCurrentValues()
 {
-    auto max = GetFDIR().ErrorCounting().MaxDevices;
+    GetTerminal().Puts("Device\tCurrent\tLimit\tInc\tDec\n");
+
+    auto& fdir = GetFDIR();
+    auto& errors = fdir.ErrorCounting();
+    auto max = errors.MaxDevices;
 
     for (decltype(max) i = 0; i < max; i++)
     {
-        GetTerminal().Printf("%d ", GetFDIR().ErrorCounting().Current(i));
+        GetTerminal().Printf("%d\t%d\t%d\t%d\t%d\n", //
+            i,                                       //
+            errors.Current(i),                       //
+            fdir.Limit(i),                           //
+            fdir.Increment(i),                       //
+            fdir.Decrement(i));
+    }
+}
+
+void ErrorCountersCommand(std::uint16_t argc, char* argv[])
+{
+    if (argc == 1 && strcmp(argv[0], "current") == 0)
+    {
+        ShowCurrentValues();
+    }
+    else
+    {
+        GetTerminal().Puts("error_counters <current>");
     }
 }
