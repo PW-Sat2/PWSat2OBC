c96357b9e81235365f6a2c907deda5347dd7252f
Łukasz Żak <zaklukasz32@gmail.com>
2017-07-31 20:11:58 +0200

    [telemetry][adcs][built-in] Request detumbling data package at least once while built-in detubmling algorithm is runnning.

--- libs/adcs/builtin/BuiltinDetumbling.cpp
+++ libs/adcs/builtin/BuiltinDetumbling.cpp
@@ -1,8 +1,21 @@
 #include "BuiltinDetumbling.hpp"
 #include <chrono>
+#include "logger/logger.h"
+
+using namespace std::chrono_literals;
 
 namespace adcs
 {
+    constexpr std::chrono::milliseconds BuiltinDetumbling::GetWaitPeriod()
+    {
+        return chrono_extensions::period_cast<std::chrono::milliseconds>(BuiltinDetumbling::Frequency);
+    }
+
+    constexpr std::chrono::seconds BuiltinDetumbling::GetDetumblingPeriod()
+    {
+        return chrono_extensions::period_cast<std::chrono::seconds>(BuiltinDetumbling::Frequency) * 2;
+    }
+
     BuiltinDetumbling::BuiltinDetumbling(devices::imtq::IImtqDriver& imtqDriver_) : imtqDriver(imtqDriver_)
     {
     }
@@ -24,11 +37,23 @@ namespace adcs
 
     void BuiltinDetumbling::Process()
     {
-        imtqDriver.StartBDotDetumbling(chrono_extensions::period_cast<std::chrono::seconds>(BuiltinDetumbling::Frequency));
+        if (!imtqDriver.StartBDotDetumbling(GetDetumblingPeriod()))
+        {
+            LOG(LOG_LEVEL_ERROR, "Unable to run built-in detumbling");
+            return;
+        }
+
+        System::SleepTask(2s);
+
+        devices::imtq::DetumbleData result;
+        if (!imtqDriver.GetDetumbleData(result))
+        {
+            LOG(LOG_LEVEL_ERROR, "Unable to acquire built-in detumble data");
+        }
     }
 
     std::chrono::milliseconds BuiltinDetumbling::GetWait() const
     {
-        return chrono_extensions::period_cast<std::chrono::milliseconds>(BuiltinDetumbling::Frequency);
+        return GetWaitPeriod();
     }
 }
