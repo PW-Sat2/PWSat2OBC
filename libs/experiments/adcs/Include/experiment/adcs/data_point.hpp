0b2de5cd7f8c54b3053d1f055f772fcc0d0c18cb
Novakov <maciejt.nowak@gmail.com>
2017-08-14 08:57:16 +0200

    Gathering detumbling experiment data point

---
+++ libs/experiments/adcs/Include/experiment/adcs/data_point.hpp
@@ -0,0 +1,26 @@
+#ifndef LIBS_EXPERIMENTS_ADCS_INCLUDE_EXPERIMENT_ADCS_DATA_POINT_HPP_
+#define LIBS_EXPERIMENTS_ADCS_INCLUDE_EXPERIMENT_ADCS_DATA_POINT_HPP_
+
+#include "gyro/telemetry.hpp"
+#include "payload/telemetry.h"
+#include "telemetry/BasicTelemetry.hpp"
+#include "telemetry/fwd.hpp"
+
+namespace experiment
+{
+    namespace adcs
+    {
+        struct DetumblingDataPoint
+        {
+            std::chrono::milliseconds Timestamp;
+            devices::payload::PayloadTelemetry::Temperatures Temperatures;
+            devices::payload::PayloadTelemetry::Photodiodes Photodiodes;
+            devices::gyro::GyroscopeTelemetry Gyro;
+            telemetry::ImtqMagnetometerMeasurements Magnetometer;
+            telemetry::ImtqDipoles Dipoles;
+            devices::payload::PayloadTelemetry::SunsRef ReferenceSunS;
+        };
+    }
+}
+
+#endif /* LIBS_EXPERIMENTS_ADCS_INCLUDE_EXPERIMENT_ADCS_DATA_POINT_HPP_ */
--- libs/telemetry/imtq/include/telemetry/ImtqTelemetryCollector.hpp
+++ libs/telemetry/imtq/include/telemetry/ImtqTelemetryCollector.hpp
@@ -21,7 +21,9 @@ namespace devices
          * of this class. They can be later extracted using dedicated method.
          * @ingroup ImtqDriver
          */
-        class ImtqTelemetryCollector final : public IImtqDriver, public telemetry::IImtqTelemetryCollector
+        class ImtqTelemetryCollector final : public IImtqDriver,
+                                             public telemetry::IImtqTelemetryCollector,
+                                             public telemetry::IImtqDataProvider
         {
           public:
             /**
@@ -96,6 +98,10 @@ namespace devices
             virtual bool ResetParameterAndGetDefault(Parameter id, gsl::span<std::uint8_t> result) final override;
 
             virtual bool CaptureTelemetry(telemetry::ManagedTelemetry& target) final override;
+
+            virtual bool GetLastMagnetometerMeasurement(telemetry::ImtqMagnetometerMeasurements& measurements) final override;
+
+            virtual bool GetLastDipoles(telemetry::ImtqDipoles& dipoles) final override;
 
             /**
              * @brief Verifies whether the requested telemetry element has been recently captured
