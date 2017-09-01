c7525c4b4852c4542239b130ece7eaebba25021c
Łukasz Żak <zaklukasz32@gmail.com>
2017-09-01 18:24:05 +0200

    [telecommand] Correctly handle version information that is longer than Downlink frame payload.

--- libs/obc/communication/telecommands/compile_info.cpp
+++ libs/obc/communication/telecommands/compile_info.cpp
@@ -15,12 +15,19 @@ namespace obc
     {
         void CompileInfoTelecommand::Handle(devices::comm::ITransmitter& transmitter, gsl::span<const std::uint8_t> /*parameters*/)
         {
-            constexpr std::uint8_t array[] = VERSION;
-            DownlinkFrame response(DownlinkAPID::CompileInfo, 0);
-            static_assert(count_of(array) <= decltype(response)::MaxPayloadSize, "Version information is too long");
-            auto& writer = response.PayloadWriter();
-            writer.WriteArray(array);
-            transmitter.SendFrame(response.Frame());
+            const auto span = gsl::ensure_z(VERSION);
+            Reader reader(gsl::make_span(reinterpret_cast<const std::uint8_t*>(span.data()), span.size()));
+            int seq = 0;
+            while (reader.RemainingSize() > 0)
+            {
+                DownlinkFrame response(DownlinkAPID::CompileInfo, seq);
+                const auto size = std::min(reader.RemainingSize(), static_cast<std::int32_t>(decltype(response)::MaxPayloadSize));
+                const auto span = reader.ReadArray(size);
+                auto& writer = response.PayloadWriter();
+                writer.WriteArray(span);
+                transmitter.SendFrame(response.Frame());
+                ++seq;
+            }
         }
     }
 }
