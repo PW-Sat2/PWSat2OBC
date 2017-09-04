ebd721d9c9b79a4594bfb6a531e1addf9ade6400
Łukasz Żak <zaklukasz32@gmail.com>
2017-09-04 21:28:42 +0200

    [telemetry][system] Update obc telemetry.
    Extend obc telemetry with operating system uptime.

---
+++ libs/telemetry/os/Include/telemetry/collect_os.hpp
@@ -0,0 +1,39 @@
+#ifndef LIBS_TELEMETRY_COLLECT_OS_HPP
+#define LIBS_TELEMETRY_COLLECT_OS_HPP
+
+#pragma once
+
+#include "mission/base.hpp"
+#include "telemetry/state.hpp"
+
+namespace telemetry
+{
+    /**
+    * @brief This task is responsible for acquiring & updating operating system telemetry
+    * @telemetry_acquisition
+    * @ingroup telemetry
+    */
+    class SystemTelemetryAcquisition : public mission::Update
+    {
+      public:
+        /**
+        * @brief dummy
+        * @param value ignored
+        */
+        SystemTelemetryAcquisition(int value);
+        /**
+        * @brief Builds update descriptor for this task.
+        * @return Update descriptor - the antenna telemetry acquisition update task.
+        */
+        mission::UpdateDescriptor<telemetry::TelemetryState> BuildUpdate();
+
+        /**
+        * @brief Updates current operating system telemetry in global state.
+        * @param[in] state Reference to global state.
+        * @param[in] param Current execution context.
+        */
+        static mission::UpdateResult UpdateProc(telemetry::TelemetryState& state, void* param);
+    };
+}
+
+#endif
