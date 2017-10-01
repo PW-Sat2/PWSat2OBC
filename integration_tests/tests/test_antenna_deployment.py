d899871608f001cff4678d5b0bc748a51ace42cb
Novakov <maciejt.nowak@gmail.com>
2017-10-01 19:32:46 +0200

    Python test for antenna deployment

--- integration_tests/tests/test_antenna_deployment.py
+++ integration_tests/tests/test_antenna_deployment.py
@@ -7,9 +7,10 @@ from utils import TestEvent
 
 
 class Test_AntennaDeployment(RestartPerTest):
-    @auto_power_on(False)
+    # @auto_power_on(False)
     def __init__(self, methodName='runTest'):
         super(Test_AntennaDeployment, self).__init__(methodName)
+        self.longMessage = True
 
     def begin_deployment(self):
         self.system.obc.jump_to_time(timedelta(minutes=42))
@@ -133,7 +134,7 @@ class Test_AntennaDeployment(RestartPerTest):
     @runlevel(1)
     @clear_state()
     def test_all_antennas_are_deployed_even_when_deployment_hangs(self):
-        list =     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         expected = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
         def primaryHandler(controller, antennaId):
@@ -158,7 +159,7 @@ class Test_AntennaDeployment(RestartPerTest):
     @runlevel(1)
     @clear_state()
     def test_antennas_are_deployed_by_backup_controller_primary_unresponsive(self):
-        list =     [0, 0, 0, 0, 0]
+        list = [0, 0, 0, 0, 0]
         expected = [1, 1, 1, 1, 1]
         self.system.i2c.enable_bus_devices([self.system.primary_antenna.address], False)
 
@@ -175,7 +176,7 @@ class Test_AntennaDeployment(RestartPerTest):
     @runlevel(1)
     @clear_state()
     def test_antennas_are_deployed_by_primary_controller_backup_unresponsive(self):
-        list =     [0, 0, 0, 0, 0]
+        list = [0, 0, 0, 0, 0]
         expected = [1, 1, 1, 1, 1]
         self.system.i2c.enable_pld_devices([self.system.backup_antenna.address], False)
 
@@ -252,3 +253,75 @@ class Test_AntennaDeployment(RestartPerTest):
 
         self.assertEqual(self.system.eps.ANTenna.was_on, True, "LCL was re-enabled")
         self.assertSequenceEqual(list, expected)
+
+    @runlevel(1)
+    @clear_state()
+    def test_full_procedure_is_performed(self):
+        self.system.i2c.enable_bus_devices([self.system.primary_antenna.address], False)
+        self.system.i2c.enable_pld_devices([self.system.backup_antenna.address], False)
+
+        def control_antenna_power(*args, **kwargs):
+            main = self.system.eps.ANTenna.is_on
+            red = self.system.eps.ANTennaRed.is_on
+            power = main or red
+            self.system.i2c.enable_bus_devices([self.system.primary_antenna.address], power)
+            self.system.i2c.enable_pld_devices([self.system.backup_antenna.address], power)
+
+        self.system.eps.ANTenna.on_enable = control_antenna_power
+        self.system.eps.ANTenna.on_disable = control_antenna_power
+        self.system.eps.ANTennaRed.on_enable = control_antenna_power
+        self.system.eps.ANTennaRed.on_disable = control_antenna_power
+
+        arm_counter = [0, 0]
+        disarm_counter = [0, 0]
+
+        primary_deploy = [False, False, False, False, False]
+        backup_deploy = [False, False, False, False, False]
+
+        def on_primary_arm_change(state):
+            if state:
+                arm_counter[0] += 1
+            else:
+                disarm_counter[0] += 1
+
+        def on_backup_arm_change(state):
+            if state:
+                arm_counter[1] += 1
+            else:
+                disarm_counter[1] += 1
+
+        def on_primary_deploy(controller, antenna_id):
+            if antenna_id == -1:
+                primary_deploy[0] = True
+            else:
+                primary_deploy[antenna_id] = True
+
+            return None
+
+        def on_backup_deploy(controller, antenna_id):
+            if antenna_id == -1:
+                backup_deploy[0] = True
+            else:
+                backup_deploy[antenna_id] = True
+
+            return None
+
+        self.system.primary_antenna.on_arm_state_change = on_primary_arm_change
+        self.system.backup_antenna.on_arm_state_change = on_backup_arm_change
+
+        self.system.primary_antenna.on_begin_deployment = on_primary_deploy
+        self.system.backup_antenna.on_begin_deployment = on_backup_deploy
+
+        t = timedelta(minutes=41)
+
+        for i in xrange(0, 100):
+            t += timedelta(seconds=60)
+
+            self.system.obc.jump_to_time(t)
+            self.system.obc.run_mission()
+
+        self.assertEqual(arm_counter, [5, 5], "Controllers should be armed 5 times")
+        self.assertEqual(disarm_counter, [5, 5], "Controllers should be disarmed 5 times")
+
+        self.assertEqual(primary_deploy, [True] * 5, "All antennas should be deployed by primary controller")
+        self.assertEqual(backup_deploy, [True] * 5, "All antennas should be deployed by backup controller")
