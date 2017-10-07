6820cd54a7a7256dd8441b5bf04f6553359a84d6
Novakov <maciejt.nowak@gmail.com>
2017-10-07 14:28:48 +0200

    Trimmed boot reason in telemetry to 16 bits

--- unit_tests/state/telemetry/SystemStartupTest.cpp
+++ unit_tests/state/telemetry/SystemStartupTest.cpp
@@ -19,23 +19,23 @@ namespace
 
     TEST(SystemStartupTest, TestCustomConstruction)
     {
-        telemetry::SystemStartup object(0x11223344, 0x55, 0x66778899);
+        telemetry::SystemStartup object(0x11223344, 0x55, 0x8899);
         ASSERT_THAT(object.BootCounter(), Eq(0x11223344u));
         ASSERT_THAT(object.BootIndex(), Eq(0x55));
-        ASSERT_THAT(object.BootReason(), Eq(0x66778899u));
+        ASSERT_THAT(object.BootReason(), Eq(0x8899u));
     }
 
     TEST(SystemStartupTest, TestSerialization)
     {
-        std::uint8_t expected[] = {0x44, 0x33, 0x22, 0x11, 0x55, 0x99, 0x88, 0x77, 0x66};
+        std::uint8_t expected[] = {0x44, 0x33, 0x22, 0x11, 0x55, 0x99, 0x88};
 
         std::array<std::uint8_t, (telemetry::SystemStartup::BitSize() + 7) / 8> buffer;
-        telemetry::SystemStartup object(0x11223344, 0x55, 0x66778899);
+        telemetry::SystemStartup object(0x11223344, 0x55, 0x8899);
         BitWriter writer(buffer);
         object.Write(writer);
         ASSERT_THAT(writer.Status(), Eq(true));
         ASSERT_THAT(writer.GetBitDataLength(), Eq(telemetry::SystemStartup::BitSize()));
-        ASSERT_THAT(writer.GetBitDataLength(), Eq(9 * 8u));
+        ASSERT_THAT(writer.GetBitDataLength(), Eq(7 * 8u));
         ASSERT_THAT(writer.Capture(), Eq(gsl::make_span(expected)));
     }
 
