abd408fb36060077865d5c65477b7af67c0b5dda
Łukasz Żak <zaklukasz32@gmail.com>
2017-10-09 21:06:08 +0200

    [beacon] Fix test failures.

--- integration_tests/tests/test_telecommands_experiments.py
+++ integration_tests/tests/test_telecommands_experiments.py
@@ -1,6 +1,6 @@
 from obc.experiments import ExperimentType
 from response_frames.common import ExperimentSuccessFrame
-from system import auto_power_on, runlevel
+from system import auto_power_on, runlevel, clear_state
 from telecommand import AbortExperiment
 from tests.base import RestartPerTest
 from utils import TestEvent
@@ -25,6 +25,7 @@ class TestExperimentsTelecommands(RestartPerTest):
         e.wait_for_change(1)
 
     @runlevel(2)
+    @clear_state()
     def test_should_abort_experiment(self):
         self._start()
 
@@ -39,6 +40,7 @@ class TestExperimentsTelecommands(RestartPerTest):
         self.system.obc.wait_for_experiment(None, 15)
 
     @runlevel(2)
+    @clear_state()
     def test_should_be_able_to_run_next_experiment_after_previous_aborted(self):
         self._start()
 
