283200815f60d9091b66b4a423438b705bf28b5d
Novakov <maciejt.nowak@gmail.com>
2017-10-12 16:18:13 +0200

    Automated test for persistent state parsing

--- integration_tests/tests/test_persistent_state.py
+++ integration_tests/tests/test_persistent_state.py
@@ -1,20 +1,44 @@
+import datetime
+from os import path
+from pprint import pprint
 from unittest import TestCase
-from persistent_state import PersistentStateParser
-import struct
+
+from build_config import config
+from persistent_state import PersistentStateParser, SailOpeningState
+
 
 class PersistentStateTest(TestCase):
+    def test_parse_generated_state(self):
+        state_file = path.join(config['REPORTS_DIR'], 'persistent_state')
+        with open(state_file, 'rb') as f:
+            data = f.read()
 
-    def test_parsing_empty_buffer(self):
-        buffer = "\0" * 273
-        result = PersistentStateParser.parse_partial(buffer)
-
-        self.assertNotEqual(result, None)
-        self.assertEqual(len(result[0]), 7)
-        self.assertTrue(result[0][0].has_key('Antenna Configuration'))
-        self.assertTrue(result[0][1].has_key('Mission Time'))
-        self.assertTrue(result[0][2].has_key('Time Correction'))
-        self.assertTrue(result[0][3].has_key('Sail State'))
-        self.assertTrue(result[0][4].has_key('Error Counters'))
-        self.assertTrue(result[0][5].has_key('Adcs Configuration'))
-        self.assertTrue(result[0][6].has_key('Periodic Message'))
-        self.assertEqual(len(result[1]), 0)
+            result = PersistentStateParser.parse(data)
+            pprint(result)
+            self.assertNotEqual(result, None)
+            self.assertEqual(len(result), 7)
+            self.assertEqual(result['Antenna Configuration'], ('Deployment Disabled', True))
+            self.assertEqual(result['Mission Time'],  {
+                'External Time': datetime.timedelta(131831949, 75728, 674000),
+                'Internal Time': datetime.timedelta(55818034, 51605, 111000)
+            })
+            self.assertEqual(result['Time Correction'], {
+                'External Clock Weight': 3344,
+                'Internal Clock Weight': 1122
+            })
+            self.assertEqual(result['Sail State'], ('Deployed', SailOpeningState.Open))
+            for k in result['Error Counters']:
+                self.assertEqual(result['Error Counters'][k], {
+                    'Counter Config': {
+                        'Decrement': 2,
+                        'Increment': 5,
+                        'Limit': 128,
+                        'Zero': 0
+                    }
+                })
+            self.assertEqual(result['Adcs Configuration'], ('Built In Detumbling Disabled', False))
+            self.assertTrue(result['Periodic Message'], {
+                'Interval': datetime.timedelta(0, 300),
+                'Message': 'We are the Borg. Lower your shields and surrender your ships. We will add your biological and technological distinctiveness to our own. Your culture will adapt to service us. Resistance is futile.\x00\x00\x00\x00',
+                'RepeatCount': 3
+            })
---
+++ test/generate_persistent_state/main.cpp
@@ -0,0 +1,82 @@
+#include <array>
+#include <cstdio>
+#include <unistd.h>
+#include "base/Writer.h"
+#include "state/LockablePersistentState.hpp"
+#include "state/StatePolicies.hpp"
+#include "state/adcs/AdcsState.hpp"
+#include "state/antenna/AntennaConfiguration.hpp"
+#include "state/antenna/AntennaState.hpp"
+#include "state/comm/MessageState.hpp"
+#include "state/fdir/ErrorCountersState.hpp"
+#include "state/fwd.hpp"
+#include "state/sail/SailState.hpp"
+#include "state/time/TimeCorrectionConfiguration.hpp"
+#include "state/time/TimeState.hpp"
+#include "system.h"
+#include "utils.h"
+
+extern "C" {
+extern void initialise_monitor_handles(void);
+extern void __libc_init_array(void);
+extern int kill(pid_t, int);
+}
+
+using namespace std::chrono_literals;
+using namespace state;
+
+void FillState(SystemPersistentState& state)
+{
+    state.Set(AntennaConfiguration(true));
+    state.Set(TimeState(0x11223344556677ms, 0x28776655443322ms));
+    state.Set(TimeCorrectionConfiguration(1122, 3344));
+    state.Set(SailState(SailOpeningState::OpeningStopped));
+    state.Set(AdcsState(false));
+}
+
+void WritePersistentStateToFile()
+{
+    auto f = fopen("persistent_state", "wb");
+
+    SystemPersistentState state;
+
+    FillState(state);
+
+    std::array<std::uint8_t, decltype(state)::Size()> buf;
+    buf.fill(0);
+
+    Writer writer(buf);
+    state.Write(writer);
+
+    fwrite(buf.data(), 1, buf.size(), f);
+
+    fclose(f);
+}
+
+OSResult System::TakeSemaphore(OSSemaphoreHandle, std::chrono::milliseconds)
+{
+    return OSResult::Success;
+}
+
+OSResult System::GiveSemaphore(OSSemaphoreHandle)
+{
+    return OSResult::Success;
+}
+
+OSSemaphoreHandle System::CreateBinarySemaphore(std::uint8_t)
+{
+    return reinterpret_cast<OSSemaphoreHandle>(1);
+}
+
+int main()
+{
+    __libc_init_array();
+
+    initialise_monitor_handles();
+
+    WritePersistentStateToFile();
+
+    kill(-1, 0);
+
+    return 0;
+}
