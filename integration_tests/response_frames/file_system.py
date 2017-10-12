ed347207abf2ecccc85ec52c412713717aaf1871
Novakov <maciejt.nowak@gmail.com>
2017-10-12 15:06:50 +0200

    Automatically decode file list

---
+++ integration_tests/response_frames/file_system.py
@@ -0,0 +1,26 @@
+import struct
+
+from response_frames import response_frame
+from response_frames.common import DownlinkApid, GenericSuccessResponseFrame
+from utils import ensure_string
+
+
+@response_frame(DownlinkApid.FileList)
+class FileListSuccessFrame(GenericSuccessResponseFrame):
+    def decode(self):
+        super(FileListSuccessFrame, self).decode()
+
+        self.file_list = []
+
+        raw = self.response
+        raw = ensure_string(raw)
+
+        while len(raw) > 0:
+            (name, raw) = raw.split('\0', 1)
+
+            size_bytes = raw[0:4]
+            (size,) = struct.unpack('<L', ensure_string(size_bytes))
+
+            raw = raw[4:]
+
+            self.file_list.append((name, size))
