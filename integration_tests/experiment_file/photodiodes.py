9aab0b617c00e18e4253db6694267ce9f76dd2f4
begreeen <michal.gumiela@gmail.com>
2017-10-30 16:06:42 +0100

    Swapped Sockets For Solar Panels On PLD Board fix (https://team.pw-sat.pl/w/ncr/ncr53/)

--- integration_tests/experiment_file/photodiodes.py
+++ integration_tests/experiment_file/photodiodes.py
@@ -1,9 +1,9 @@
 from base import uint16, count, label_as, pid, to_dict, field, joint
 
 Photodiodes = pid(0x33) >> joint(
-    field('Xp', uint16),
     field('Xn', uint16),
-    field('Yp', uint16),
+    field('Xp', uint16),
     field('Yn', uint16),
+    field('Yp', uint16),
 ).bind(to_dict)
 Photodiodes >>= label_as('Photodiodes')
