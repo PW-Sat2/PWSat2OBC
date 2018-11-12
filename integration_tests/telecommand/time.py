8b6c47faddcdc9bfce320a8f349cef7dddd8d93c
Łukasz Żak <lzak@future-processing.com>
2018-11-12 22:47:59 +0100

    Minor telecommand fixes.
    * Update time related telecommands to conform the convention regarding the
    correlation id
    * Make time related telecommands more analysis friendly

--- integration_tests/telecommand/time.py
+++ integration_tests/telecommand/time.py
@@ -1,30 +1,30 @@
 import struct
 from datetime import timedelta
 
-from telecommand.base import Telecommand
+from telecommand.base import CorrelatedTelecommand
 from utils import ensure_byte_list
 
 
-class SetTimeCorrectionConfig(Telecommand):
+class SetTimeCorrectionConfig(CorrelatedTelecommand):
     def __init__(self, correlation_id, missionTimeWeight, externalTimeWeight):
-        self._correlation_id = correlation_id
-        self._missionTimeWeight = ensure_byte_list(struct.pack('<H', missionTimeWeight))
-        self._externalTimeWeight = ensure_byte_list(struct.pack('<H', externalTimeWeight))
+        super(SetTimeCorrectionConfig, self).__init__(correlation_id)
+        self._missionTimeWeight = missionTimeWeight
+        self._externalTimeWeight = externalTimeWeight
 
     def apid(self):
         return 0x90
 
     def payload(self):
-        return [self._correlation_id] + self._missionTimeWeight + self._externalTimeWeight
+        return struct.pack('<BHH', self._correlation_id, self._missionTimeWeight, self._externalTimeWeight)
 
-class SetTime(Telecommand):
+class SetTime(CorrelatedTelecommand):
     def __init__(self, correlation_id, new_time):
+        super(SetTime, self).__init__(correlation_id)
         # type: (int, timedelta) -> None
-        self._correlation_id = correlation_id
-        self._newTime = ensure_byte_list(struct.pack('<I', new_time.seconds))
+        self._newTime = new_time
 
     def apid(self):
         return 0x91
 
     def payload(self):
-        return [self._correlation_id] + self._newTime
+        return struct.pack('<BI', self._correlation_id, self._newTime.seconds)
