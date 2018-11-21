ade13f5b1e0a6d7cea33e8a23e46a99b25f178a9
begreeen <michal.gumiela@gmail.com>
2018-11-22 00:54:41 +0100

    style fixes

--- integration_tests/tools/just_mocks.py
+++ integration_tests/tools/just_mocks.py
@@ -128,7 +128,7 @@ Arguments may also be stored in file and passed with '@file' syntax. Arguments i
 
 class JustMocks(object):
     def __init__(self, mock_com, enable_zmq, grc_uplink_address="tcp://localhost:7002", grc_downlink_address="tcp://localhost:7003",
-    	uplink_per=0, downlink_per=0):
+        uplink_per=0, downlink_per=0):
         self._mock_com = mock_com
 
         self.i2c = I2CMock(mock_com)
@@ -161,7 +161,7 @@ class JustMocks(object):
 
         if enable_zmq:
             self.zmq_adapter = ZeroMQAdapter(self.comm, grc_uplink_address=grc_uplink_address, grc_downlink_address=grc_downlink_address,
-            								 uplink_per=uplink_per, downlink_per=downlink_per)
+                                             uplink_per=uplink_per, downlink_per=downlink_per)
 
     def start(self, devices_to_enable):
         self.i2c.start(enable_devices=False)
@@ -199,7 +199,7 @@ config = imp.load_source('config', args.config)
 devices_to_enable = {k: AVAILABLE_DEVICES[k] for k in args.enabled_devices}
 
 just_mocks = JustMocks(mock_com=config.config['MOCK_COM'], enable_zmq=not args.disable_zmq, grc_uplink_address=args.grc_uplink_address, grc_downlink_address=args.grc_downlink_address,
-					   uplink_per=float(args.uplink_per), downlink_per=float(args.downlink_per))
+                       uplink_per=float(args.uplink_per), downlink_per=float(args.downlink_per))
 
 print 'Starting devices: {}'.format(', '.join(sorted(devices_to_enable.keys())))
 
