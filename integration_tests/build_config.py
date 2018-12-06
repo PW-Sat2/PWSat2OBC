a001f85665239555dbf5c0719a6a60d8c4be91a5
Maciej Nowak <maciejt.nowak@gmail.com>
2018-12-06 01:32:59 +0100

    Sorry...

--- integration_tests/build_config.py
+++ integration_tests/build_config.py
@@ -3,7 +3,6 @@ import sys
 try:
     from config import config
 except ImportError as e:
-    print sys.modules
     raise ImportError(
         "Error loading config: %s. Did you forget to add <build>/integration_tests to PYTHONPATH?" % e.message)
 
