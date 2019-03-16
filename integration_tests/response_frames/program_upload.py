382c00ba7aa7243a0f75d3c7a8dde7576af1c6bd
Tomasz Martyniak <tom4r@users.noreply.github.com>
2019-03-16 18:03:08 +0100

    Added decoding Upload Error Frames.

--- integration_tests/response_frames/program_upload.py
+++ integration_tests/response_frames/program_upload.py
@@ -46,3 +46,72 @@ class CopyBootSlots(ResponseFrame):
     @classmethod
     def matches(cls, payload):
         return True
+
+
+@response_frame(0x04)
+class EntryEraseError(ResponseFrame):
+    @classmethod
+    def matches(cls, payload):
+        return len(payload) == 8 and payload[0:2] == [0, 1]
+
+    def decode(self):
+        (_, _, self.code, self.entry, self.offset) = struct.unpack('<BBBBI', ensure_string(self.payload()))
+
+    def __repr__(self):
+        return "{}: Seq={:02d} Code={} Entry={} @ 0x{:X}".format(self.__class__.__name__, self._seq, self.code, self.entry, self.offset)
+
+
+@response_frame(0x04)
+class EntryEraseMalformedError(ResponseFrame):
+    @classmethod
+    def matches(cls, payload):
+        return len(payload) == 3 and payload[0:2] == [0, 1, 10]
+
+
+@response_frame(0x04)
+class EntryProgramPartWriteError(ResponseFrame):
+    @classmethod
+    def matches(cls, payload):
+        return len(payload) == 8 and payload[0:2] == [1, 1]
+
+    def decode(self):
+        (_, _, self.code, self.entry, self.offset) = struct.unpack('<BBBBI', ensure_string(self.payload()))
+
+    def __repr__(self):
+        return "{}: Seq={:02d} Code={} Entry={} @ 0x{:X}".format(self.__class__.__name__, self._seq, self.code, self.entry, self.offset)
+
+@response_frame(0x04)
+class EntryProgramPartWriteMalformedError(ResponseFrame):
+    @classmethod
+    def matches(cls, payload):
+        return len(payload) == 3 and payload[0:2] == [1, 1, 10]
+
+@response_frame(0x04)
+class EntryFinalizeError(ResponseFrame):
+    @classmethod
+    def matches(cls, payload):
+        return len(payload) == 4 and payload[0:2] == [2, 1]
+
+    def decode(self):
+        (_, _, self.code, self.entry) = struct.unpack('<BBBB', ensure_string(self.payload()))
+
+    def __repr__(self):
+        return "{}: Seq={:02d} Code={} Entry={}".format(self.__class__.__name__, self._seq, self.code, self.entry)
+
+@response_frame(0x04)
+class EntryFinalizeCRCError(ResponseFrame):
+    @classmethod
+    def matches(cls, payload):
+        return len(payload) == 6 and payload[0:2] == [2, 20]
+
+    def decode(self):
+        (_, _, self.entry, self.crc) = struct.unpack('<BBBH', ensure_string(self.payload()))
+
+    def __repr__(self):
+        return "{}: Seq={:02d} Entry={} CRC=0x{:X}".format(self.__class__.__name__, self._seq, self.entry, self.crc)
+
+@response_frame(0x04)
+class EntryFinalizeMalformedError(ResponseFrame):
+    @classmethod
+    def matches(cls, payload):
+        return len(payload) == 3 and payload[0:2] == [2, 1, 10]
